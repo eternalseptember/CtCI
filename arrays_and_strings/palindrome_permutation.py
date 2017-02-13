220fdbe252628ba46fe849e2d2b050a3e9082e7b
KQ <eternal.september@gmail.com>
2017-02-12 23:12:37 -0500

    starting palindrome permutation

---
+++ arrays_and_strings/palindrome_permutation.py
@@ -0,0 +1,21 @@
+"""
+Given a string, write a function to check if it is a permutation
+of a palindrome. A palindrome is a word or phrase that is the same
+forwards and backwards. A permutation is a rearrangement of
+letters. The palindrome dos nto need to be limited to just
+dictionary words.
+"""
+
+
+def list_permutations(str):
+	permutations = []
+
+	return permutations
+
+
+
+inp = 'Tact Coa'
+permutations = list_permutations(inp)
+print(permutations)
+
+
