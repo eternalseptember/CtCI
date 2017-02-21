f8fa98b04fb63273f6ced941d9b74db3bfc33884
KQ <eternal.september@gmail.com>
2017-02-21 15:33:50 -0500

    string compression problem done

---
+++ arrays_and_strings/string_compression.py
@@ -0,0 +1,44 @@
+"""
+Implement a method to perform basic string compression using the counts of
+repeated characters. If the "compressed" string would not become smaller
+than the original string, your method should return the original string.
+You can assume the string has ony uppercase and lowercase letters(a-z).
+"""
+
+
+def compress(orig_str):
+	str_len = len(orig_str)
+
+	letter = None
+	count = 1
+	comp_str = ''
+
+	for char in orig_str:
+		if letter is None:
+			letter = char
+		elif letter is char:
+			count += 1
+		else:
+			append = letter + str(count)
+			comp_str += append
+
+			letter = char
+			count = 1
+
+	# after the for loop is done, append the last set
+	append = letter + str(count)
+	comp_str += append
+
+	if str_len < len(comp_str):
+		return orig_str
+	else:
+		return comp_str
+
+
+# expected answer: a2b1c5a3, abcde
+inp = ['aabcccccaaa', 'abcde']
+
+for string in inp:
+	print(compress(string))
+
+
