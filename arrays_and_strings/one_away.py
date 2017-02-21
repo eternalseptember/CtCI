4d025135c197948081aa0d89927e0ada28ace6a0
KQ <eternal.september@gmail.com>
2017-02-21 14:40:32 -0500

    seems to work

--- arrays_and_strings/one_away.py
+++ arrays_and_strings/one_away.py
@@ -7,31 +7,25 @@ edit (or zero edits) away.
 
 
 def one_away(str1, str2):
-	return check_replace(str1, str2)
+	str1_len = len(str1)
+	str2_len = len(str2)
 
-	"""
-	if check_replace(str1, str2):
-		return True
-	elif check_insert(str1, str2):
-		return True
-	elif check_remove(str1, str2):
-		return True
+	if str1_len == str2_len:
+		return check_replace(str1, str2)
+	elif (str2_len - str1_len) == 1:
+		return check_insert(str1, str2)
+	elif (str1_len - str2_len) == 1:
+		return check_remove(str1, str2)
 	else:
 		return False
-	"""
 
 
 def check_replace(str1, str2):
-	# string length should be equal
 	# only one character should be changed
-
-	if len(str1) != len(str2):
-		return False
-
 	diff = 0
-	num_chars = len(str1)
+	str_len = len(str1)
 
-	for index in range(num_chars):
+	for index in range(str_len):
 		if str1[index] is not str2[index]:
 			diff += 1
 
@@ -42,25 +36,54 @@ def check_replace(str1, str2):
 
 
 def check_insert(str1, str2):
-	# string sizes could also equal
 	# return True if str2 inserted a character
-	# and no other characters are have been changed
-	return True
+	diff = 0
+	str2_len = len(str2)
+	str1_index = 0
+
+	for str2_index in range(str2_len):
+		if (str1_index + 1) == str2_len:
+			# str1_index would be out of bounds
+			# checks if the insertion happened at the end
+			diff += 1
+		elif str1[str1_index] == str2[str2_index]:
+			str1_index += 1
+		else:
+			diff += 1
+
+	if diff == 1:
+		return True
+	else:
+		return False
 
 
 def check_remove(str1, str2):
-	# string sizes could also equal
 	# return True if str2 removed a character
-	return True
+	diff = 0
+	str1_len = len(str1)
+	str2_index = 0
 
+	for str1_index in range(str1_len):
+		if (str2_index + 1) == str1_len:
+			# str2_index would be out of bounds
+			# checks if the last letter was deleted
+			diff += 1
+		elif str1[str1_index] is not str2[str2_index]:
+			diff += 1
+		else:
+			str2_index += 1
 
+	if diff == 1:
+		return True
+	else:
+		return False
 
 
 # true, true, true, false
-str_1_set = ['pale', 'pales', 'pale', 'pale']
-str_2_set = ['ple', 'pale', 'bale', 'bake']
+str_1_set = ['pale', 'pales', 'pale', 'pale', 'pale']
+str_2_set = ['ple', 'pale', 'bale', 'bake', 'pales']
 
-sets = 4
+sets = len(str_1_set)
 for i in range(sets):
 	str_1 = str_1_set[i]
 	str_2 = str_2_set[i]
