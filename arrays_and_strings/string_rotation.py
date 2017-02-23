bcc6486484f769922a7a3a61d197b9957c77bf2c
KQ <eternal.september@gmail.com>
2017-02-23 14:15:48 -0500

    finished string rotation

--- arrays_and_strings/string_rotation.py
+++ arrays_and_strings/string_rotation.py
@@ -6,11 +6,25 @@ check if s2 is a rotation of s1 using only one call to isSubstring.
 
 
 def is_substring(str1, str2):
-	return True
+	if len(str1) != len(str2):
+		return False
+
+	length = len(str2)
+	for index in range(1, length):
+		part1 = str1[:index]
+		part2 = str1[index:]
+		new_string = part2 + part1
+		
+		if new_string == str2:
+			return True
+
+	return False
 
 
 # print(is_substring('', ''))  #
 print(is_substring('waterbottle', 'erbottlewat'))  # True
 print(is_substring('nap', 'pan'))  # False
 print(is_substring('racecar', 'cecarra'))  # True
+print(is_substring('ring', 'ing'))  # False
+
 
