fa4c610de83fd3001be9eff62ffbaecae9312a1d
KQ <eternal.september@gmail.com>
2017-02-24 16:51:24 -0500

    setting up linked list problem

---
+++ linked_lists/remove_dupe.py
@@ -0,0 +1,19 @@
+"""
+Write code to remove duplicates from an unsorted linked list.
+How would you solve this problem if a temporary buffer is not allowed?
+"""
+
+
+class Node:
+	def __init__(self, data=None, prev=None, next_node=None):
+		self.data = data
+		self.prev = prev
+		self.next = next_node
+
+
+
+
+# Setup for testing
+node_a = Node(1)
+
+
