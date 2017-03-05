abb5c5f9b5d09380bb5272126117493673cd21db
KQ <eternal.september@gmail.com>
2017-03-04 19:23:27 -0500

    I think this outputs the correct result

--- linked_lists/return_kth_to_last.py
+++ linked_lists/return_kth_to_last.py
@@ -15,6 +15,48 @@ def print_linked_list(head):
 		print_linked_list(head.next)
 
 
+def add(head, value):
+	if head is None:
+		return Node(value)
+
+	if head.next is None:
+		head.next = Node(value)
+	else:
+		next_node = head.next
+		while next_node.next is not None:
+			next_node = next_node.next
+		next_node.next = Node(value)
+
+	return head
+
+
+def find_element(k, head):
+	items = []
+
+	while head.next is not None:
+		items.append(head.data)
+		head = head.next
+
+	if len(items) < k:
+		return None
+	else:
+		return items[-k]
+
+
+values = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+head = None
+
+for value in values:
+	head = add(head, value)
+
+print('linked list adding all nodes:')
+print_linked_list(head)
+print()
+
+k = 2
+result = find_element(k, head)
+print('{0}-to-last element in linked list:'.format(k))
+print(result)
 
 
 
