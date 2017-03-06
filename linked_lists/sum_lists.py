24571509d567adbef0516d349269af51e341d1b4
KQ <eternal.september@gmail.com>
2017-03-05 20:08:48 -0500

    setting up

--- linked_lists/sum_lists.py
+++ linked_lists/sum_lists.py
@@ -19,5 +19,44 @@ def print_linked_list(head):
 		print_linked_list(head.next)
 
 
+def add(head, value):
+	if head is None:
+		return Node(value)
+
+	if head.next is None:
+		head.next = Node(value)
+	else:
+		next_node = head.next
+		while next_node.next is not None:
+			next_node = next_node.next
+		next_node.next = Node(value)
+
+	return head
+
+
+def add_linked_list_digits(head1, head2):
+	# convert each linked list to number representation
+	total = 0
+	return total
+
+
+
+# total should be 62
+value1 = [0, 1]  # 10
+value2 = [2, 5]  # 52
+
+head1 = None
+head2 = None
+
+for value in value1:
+	head1 = add(head1, value)
+
+for value in value2:
+	head2 = add(head2, value)
+
+total = add_linked_list_digits(head1, head2)
+print(total)
+
+
 
 
