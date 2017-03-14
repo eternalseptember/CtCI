97813371ec3ec04078b6119f8bf00f541e1dba1c
KQ <eternal.september@gmail.com>
2017-03-13 21:42:10 -0400

    looks like intersection can be found

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -40,7 +40,7 @@ def find_intersection(head1, head2):
 
 	while head1 is not None:
 		while head2 is not None:
-			if (head1.data == head2.data) and (head1.next == head2.next):
+			if (head1.data == head2.data) and (head1.next.data == head2.next.data):
 				return head2
 			head2 = head2.next
 		head1 = head1.next
