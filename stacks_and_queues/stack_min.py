7ed7515bf95e99c4e50406d4cbc3894e6721e8d6
KQ <eternal.september@gmail.com>
2017-03-14 20:13:13 -0400

    setting up stacks and queues questions

---
+++ stacks_and_queues/stack_min.py
@@ -0,0 +1,44 @@
+"""
+How would you design a stack which, in addition to push and pop,
+has a functin min which returns the minimum element? Push, pop,
+and min should all operate in O(1) time.
+"""
+
+
+class Stack:
+	stack = []
+	min_item = None
+
+	def push(self, item):
+		self.stack.append(item)
+
+		if self.min_item is None:
+			self.min_item = item
+		else:
+			if item < self.min_item:
+				self.min_item = item
+
+	def pop(self):
+		item = self.stack.pop()
+
+		if item == self.min_item:
+			if len(self.stack) > 0:
+				self.min_item = min(self.stack)
+			else:
+				self.min_item = None
+
+		return item
+
+	def print_min(self):
+		print(self.min_item)
+
+
+# test
+values_to_add = [5, 6, 1, 2, 8, 0, 9]
+
+new_stack = Stack()
+for value in values_to_add:
+	new_stack.push(value)
+	new_stack.print_min()
+
+
---
+++ stacks_and_queues/three_in_one.py
@@ -0,0 +1,8 @@
+"""
+Describe how you could use a single array to implement three stacks.
+"""
+
+
+# keep track of the indexes for the beginning and ending of each stack
+three_stacks = []
+
