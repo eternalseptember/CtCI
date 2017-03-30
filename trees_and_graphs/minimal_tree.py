e71391283ddf4947c4bc7f3a9bf5e98d635e2fe0
KQ <eternal.september@gmail.com>
2017-03-29 22:39:30 -0400

    setting up and adding ways to check answer

--- trees_and_graphs/minimal_tree.py
+++ trees_and_graphs/minimal_tree.py
@@ -12,16 +12,43 @@ class Node:
 
 
 	def __str__(self):
-		return 'data: {0}  left: {1}  right: {2}'.format(self.data, self.left, self.right)
+		left = None
+		right = None
+
+		if self.left is not None:
+			left = self.left.data
+		if self.right is not None:
+			right = self.right.data
+
+		return 'data: {0}  left: {1}  right: {2}'.format(self.data, left, right)
 
 
+def print_tree(head):
+	if head.left is not None:
+		print_tree(head.left)
+	print(head)
+	if head.right is not None:
+		print_tree(head.right)
+
+
+def create_BST(data):
+	if head is None:
+		return Node(data)
 
 
 # testing
 values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+head = None
 
-node1 = Node(1)
-print(node1)
+for value in values:
+	head = create_bst(value)
+
+
+
+
+
+
+
 
 
 
