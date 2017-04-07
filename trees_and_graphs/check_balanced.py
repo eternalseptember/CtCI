a602ecefc925b821ca0090c76fbe03bdbc0350b5
KQ <eternal.september@gmail.com>
2017-04-06 22:25:11 -0400

    more test cases

--- trees_and_graphs/check_balanced.py
+++ trees_and_graphs/check_balanced.py
@@ -42,7 +42,13 @@ print(is_binary(node1))
 
 
 # false: unbalanced
-
+node1 = Node(1)
+node2 = Node(2, None, node1)
+node3 = Node(3)
+node4 = Node(4, node3, node2)
+node5 = Node(5)
+node6 = Node(6, node5, node4)
+print(is_binary(node6))
 
 
 # true: balanced
@@ -58,6 +64,10 @@ print(is_binary(node4))
 
 
 # true: balanced
+node1 = Node(1)
+node2 = Node(2, node1)
+print(is_binary(node2))
+
 
 
 
