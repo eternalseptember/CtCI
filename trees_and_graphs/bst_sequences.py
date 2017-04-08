c50231091a31e65c90fdb72485876e1a560aee0a
KQ <eternal.september@gmail.com>
2017-04-07 23:55:09 -0400

    setting up bst sequences problem

---
+++ trees_and_graphs/bst_sequences.py
@@ -0,0 +1,30 @@
+"""
+A binary search tree was created by traversing through an array from
+left to right and inserting each element. Given a binary search tree
+with distinct elements, print all possible arrays that could have led
+to this tree.
+"""
+
+
+class Node:
+	def __init__(self, data=None, left=None, right=None):
+		self.data = data
+		self.left = left
+		self.right = right
+
+
+def bst_sequences(head):
+	# return something
+
+
+# testing
+node1 = Node(1)
+node3 = Node(3)
+node2 = Node(2, node1, node3)
+
+results = bst_sequences(node2)
+# results: {2, 1, 3}, {2, 3, 1}
+
+
+
+
