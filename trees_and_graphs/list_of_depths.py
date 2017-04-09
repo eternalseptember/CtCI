f78013e8314f7d1b3b629930b5c013621240d350
KQ <eternal.september@gmail.com>
2017-04-08 23:39:44 -0400

    setting up for list of depths problem

---
+++ trees_and_graphs/list_of_depths.py
@@ -0,0 +1,22 @@
+"""
+Given a binary tree, design an algorithm which creates a linked list of
+all the nodes at each depth (e.g., if you have a tree with depth D,
+you'll have D linked lists).
+"""
+
+
+class Node:
+	def __init__(self, data=None, left=None, right=None):
+		self.data = data
+		self.left = left
+		self.right = right
+
+
+def list_of_depths(head):
+	# return something
+
+
+# testing
+
+
+
