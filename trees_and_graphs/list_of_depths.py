c9bd87c854a9dd147611de0b9996157b06436ba3
KQ <eternal.september@gmail.com>
2017-04-09 23:05:50 -0400

    test cases

--- trees_and_graphs/list_of_depths.py
+++ trees_and_graphs/list_of_depths.py
@@ -14,9 +14,27 @@ class Node:
 
 def list_of_depths(head):
 	# return something
+	return None
 
 
-# testing
+# testing; depth: 3
+node8 = Node(8)
+node9 = Node(9)
+node5 = Node(5, node8, node9)
+node4 = Node(4)
+node2 = Node(2, node4, node5)
+node6 = Node(6)
+node7 = Node(7)
+node3 = Node(3, node6, node7)
+node1 = Node(1, node2, node3)
 
+
+
+# testing: depth 4
+node5 = Node(5)
+node4 = Node(4, None, node5)
+node3 = Node(3, node4)
+node2 = Node(2, None, node3)
+node1 = Node(1, None, node2)
 
 
