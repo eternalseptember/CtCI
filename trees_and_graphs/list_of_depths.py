75bf220e38710a700ec0932b7f930e97a5c34663
KQ <eternal.september@gmail.com>
2017-04-10 23:11:27 -0400

    sort of getting there... popping from an empty list error...

--- trees_and_graphs/list_of_depths.py
+++ trees_and_graphs/list_of_depths.py
@@ -13,8 +13,31 @@ class Node:
 
 
 def list_of_depths(head):
-	# return something
-	return None
+	if head is None:
+		return None
+
+	linked_lists = {}
+	queue = [(head, 0)]
+
+	while queue is not None:
+		item = queue.pop(0)
+		current_node, depth = (item)
+
+		if depth not in linked_lists:
+			linked_lists[depth] = [current_node.data]
+		else:
+			linked_lists[depth].append(current_node.data)
+
+		if current_node.left is not None:
+			queue.append((current_node.left, depth+1))
+		if current_node.right is not None:
+			queue.append((current_node.right, depth+1))
+
+
+	return linked_lists
+
+
+
 
 
 # testing; depth: 3
@@ -28,7 +51,8 @@ node7 = Node(7)
 node3 = Node(3, node6, node7)
 node1 = Node(1, node2, node3)
 
-
+lists = list_of_depths(node1)
+print(lists)
 
 # testing: depth 4
 node5 = Node(5)
@@ -36,5 +60,8 @@ node4 = Node(4, None, node5)
 node3 = Node(3, node4)
 node2 = Node(2, None, node3)
 node1 = Node(1, None, node2)
+
+lists = list_of_depths(node1)
+print(lists)
 
 
