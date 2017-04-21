0ac53f528c5826853073088d5d6c4cfb3905af2c
KQ <eternal.september@gmail.com>
2017-04-20 23:35:57 -0400

    each node should have a link to its parent

--- trees_and_graphs/first_common_ancestor.py
+++ trees_and_graphs/first_common_ancestor.py
@@ -6,14 +6,16 @@ NOTE: This is not necessarily a binary search tree.
 
 
 class Node:
-	def __init__(self, data=None, left=None, right=None):
+	def __init__(self, data=None, left=None, right=None, parent=None):
 		self.data = data
 		self.left = left
 		self.right = right
+		self.parent = parent
 
 
 def common_ancestor(node1, node2):
 	# return something
+
 
 
 # testing
@@ -21,12 +23,21 @@ node9 = Node(9)
 node1 = Node(1)
 node2 = Node(2)
 node12 = Node(12, node2)
+node2.parent = node12
 node7 = Node(7, node1, node12)
+node1.parent = node7
+node12.parent = node7
 node5 = Node(5, node9, node7)
+node9.parent = node5
+node7.parent = node5
 node3 = Node(3)
 node11 = Node(11, node3)
+node3.parent = node11
 node4 = Node(4, None, node11)
+node11.parent = node4
 node8 = Node(8, node5, node4)
+node5.parent = node8
+node4.parent = node8
 
 
 
