39d4abbd73aa779683431bdd0251a1efdf56aba6
KQ <eternal.september@gmail.com>
2017-07-19 18:14:07 -0400

    might be returning correct answer?

--- trees_and_graphs/bst_sequences.py
+++ trees_and_graphs/bst_sequences.py
@@ -13,46 +13,54 @@ class Node:
 		self.right = right
 
 
-def bst_sequences(head, prev_sequences=[]):
+def bst_sequences(head):
 	if head is None:
 		return None
 
 	# root is the very first value that must be in every array
-	prev_sequences.append(head.data)
+	beg_sequence = [head.data]
 
-	if head.left is None and head.right is None:
-		return prev_sequences
+	if (head.left is None) and (head.right is None):
+		return beg_sequence
 
-	all_sets = []
+	# finds the possible sequences of left and right subtrees
 	left = None
 	right = None
 
 	if head.left is not None:
-		left = bst_sequences(head.left, prev_sequences)
-
+		left = bst_sequences(head.left)
 	if head.right is not None:
-		right = bst_sequences(head.right, prev_sequences)
+		right = bst_sequences(head.right)
 
-	if (left is not None) and (right is not None):
-		ltr = []
-		rtl = []
+	# creates all possible sequences for entire tree
+	possible_sets = []
 
-		ltr.append(left)
-		ltr.append(right)
-
-		rtl.append(right)
-		rtl.append(left)
-	elif left is not None:
-		return left
+	if right is None:
+		# left only
+		possible_sets.append(left)
+	elif left is None:
+		# right only
+		possible_sets.append(right)
 	else:
-		return right
+		# ltr
+		possible_sets.append(left + right)
+		# rtl
+		possible_sets.append(right + left)
 
-	return all_sets
+
+	# prepend the beg_sequence
+	all_possible_sets = []
+
+	for each_set in possible_sets:
+
+		new_seq = beg_sequence + each_set
+		all_possible_sets.append(new_seq)
+
+	return all_possible_sets
 
 
 
-
-# testing: {2, 1, 3}, {2, 3, 1}
+# Test Case 1: {2, 1, 3}, {2, 3, 1}
 node1 = Node(1)
 node3 = Node(3)
 node2 = Node(2, node1, node3)
@@ -60,8 +68,24 @@ node2 = Node(2, node1, node3)
 results = bst_sequences(node2)
 print(results)
 
-"""
-# testing: 
+
+# Test Case 2 (left node): {2, 1}
+node1 = Node(1)
+node2 = Node(2, node1)
+
+results = bst_sequences(node2)
+print(results)
+
+
+# Test Case 3 (right node): {2, 1}
+node1 = Node(1)
+node2 = Node(2, None, node1)
+
+results = bst_sequences(node2)
+print(results)
+
+
+# Test Case 4: [{2, 1, 4, 3}, {2, 3, 1, 4}] ??
 node4 = Node(4)
 node1 = Node(1, node4)
 node3 = Node(3)
@@ -69,7 +93,9 @@ node2 = Node(2, node1, node3)
 
 results = bst_sequences(node2)
 print(results)
-"""
+
+
+
 
 
 
