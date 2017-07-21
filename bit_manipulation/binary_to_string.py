ebf8fcdb1af9ce86280bda01f4d393b285d65bf2
KQ <eternal.september@gmail.com>
2017-07-21 18:29:01 -0400

    starting bit manipulation problems

---
+++ bit_manipulation/binary_to_string.py
@@ -0,0 +1,26 @@
+"""
+Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a
+double, print the binary representation. If the number cannot be
+represented accurately in binary with at most 32 characters, print "ERROR".
+"""
+
+
+def binary_to_string(num):
+	bin_string = ''
+
+	return bin_string
+
+
+# Test case 1: how to approach this problem using integers
+inp = 2
+print(binary_to_string(inp))
+
+"""
+# Test case 2
+inp = 0.72
+print(binary_to_string(inp))
+"""
+
+
+
+
