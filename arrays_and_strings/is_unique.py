ece8e548d044c206c90fa4bb819e63350dbd3c49
KQ <eternal.september@gmail.com>
2017-08-05 22:26:09 -0400

    going through answer key

--- arrays_and_strings/is_unique.py
+++ arrays_and_strings/is_unique.py
@@ -5,6 +5,12 @@ characters. What if you cannot use additional data structures?
 
 
 def check_characters(str):
+	# ASCII has 128 characters
+	if len(str) > 128:
+		return False
+	# extended ASCII has 256 characters
+
+
 	already_stored = {}
 	for char in str:
 		if char not in already_stored:
