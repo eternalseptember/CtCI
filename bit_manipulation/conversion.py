6edb3813dc2ffa0f37c12d9562cea74bfc84c738
KQ <eternal.september@gmail.com>
2017-08-15 16:45:59 -0400

    formatting the binary string for comparison

--- bit_manipulation/conversion.py
+++ bit_manipulation/conversion.py
@@ -5,15 +5,30 @@ flip to convert integer A to integer B.
 
 
 def number_of_bits_to_flip(int_a, int_b):
-	num_to_flip = 0
-
 	bin_a = int_to_binary(int_a)
 	bin_b = int_to_binary(int_b)
+
+	# pad leading zeros
+	bin_a_len = len(bin_a)
+	bin_b_len = len(bin_b)
+
+	if bin_a_len > bin_b_len:
+		# pad bin_b
+		padding_zeros = bin_a_len - bin_b_len
+		for i in range(padding_zeros):
+			bin_b.insert(0, 0)
+	elif bin_b_len > bin_a_len:
+		# pad bin_a
+		padding_zeros = bin_b_len - bin_a_len
+		for i in range(padding_zeros):
+			bin_a.insert(0, 0)
 
 	print(bin_a)
 	print(bin_b)
 
-	# pad leading zeros
+	# lengths should be equal
+	num_to_flip = 0
+
 
 	return num_to_flip
 
