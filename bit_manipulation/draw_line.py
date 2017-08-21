f545da3b1d1d3a3c3ccc73adc5675db7f8ccff11
KQ <eternal.september@gmail.com>
2017-08-21 16:03:25 -0400

    setting up draw line problem

---
+++ bit_manipulation/draw_line.py
@@ -0,0 +1,33 @@
+"""
+A monochrome screen is stored as a single array of bytes, allowing eight
+consecutive pixels to be stored in one byte. The screen has width w,
+where w is divisible by 8 (that is, no byte will be split across rows).
+The height of the screen, of course, can be derived from the length of
+the array and the width. Implement a function that draws a horizontal
+line from (xl, y) to (x2, y).
+
+The method signature should look something like:
+drawLine(byte[] screen, int width, int xl, int x2, int y)
+"""
+
+
+def draw_line(screen, width, x1, x2, y):
+	# length and height are synonomous in this problem
+	height = len(screen)
+	
+	return None
+
+
+# Testing
+# _ is a blank placeholder; represents 0
+"""
+________ : 0
+__xxxx__ : 
+________ : 0
+________ : 0
+"""
+
+draw_line()
+
+
+
