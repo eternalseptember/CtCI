7a989ce37f2bdc6396eb729b63c0b4645e5a19b7
KQ <eternal.september@gmail.com>
2017-09-03 14:23:55 -0400

    reformatted test cases and finished refactoring adder

--- linked_lists/sum_lists.py
+++ linked_lists/sum_lists.py
@@ -8,115 +8,133 @@ sum as a linked list.
 
 
 class Node:
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next = next_node
+	def __init__(self, data=None, next_node=None):
+		self.data = data
+		self.next = next_node
 
 
 def print_linked_list(head):
-    print(head.data, end=' ')
-    if head.next is not None:
-        print_linked_list(head.next)
+	print(head.data, end=' ')
+	if head.next is not None:
+		print_linked_list(head.next)
 
 
 def add(head, value):
-    if head is None:
-        return Node(value)
+	if head is None:
+		return Node(value)
 
-    if head.next is None:
-        head.next = Node(value)
-    else:
-        next_node = head.next
-        while next_node.next is not None:
-            next_node = next_node.next
-        next_node.next = Node(value)
+	if head.next is None:
+		head.next = Node(value)
+	else:
+		next_node = head.next
+		while next_node.next is not None:
+			next_node = next_node.next
+		next_node.next = Node(value)
 
-    return head
+	return head
 
 
 def convert_to_linked_list(num):
-    # convert num to a linked list with the digits stored in reverse order
-    digit_list = [int(digit) for digit in str(num)]
-    digit_list = digit_list[::-1]
+	# convert num to a linked list with the digits stored in reverse order
+	digit_list = [int(digit) for digit in str(num)]
+	digit_list = digit_list[::-1]
 
-    head = None
-    for value in digit_list:
-        head = add(head, value)
+	head = None
+	for value in digit_list:
+		head = add(head, value)
 
-    return head
+	return head
 
 
 def add_linked_list_nums(head1, head2):
-    # Not completely sure if this is a valid assumption, but
-    # if either head is none, treat them as if they're zeros.
-    if (head1 is None) and (head2 is None):
-        return Node(0)
-    elif (head1 is None):
-        return head2
-    elif (head2 is None):
-        return head1
+	# Not completely sure if this is a valid assumption, but
+	# if either head is none, treat them as if they're zeros.
+	if (head1 is None) and (head2 is None):
+		return Node(0)
+	elif (head1 is None):
+		return head2
+	elif (head2 is None):
+		return head1
 
 
-    # Setup
-    first_digit = None
-    current_digit = None
-    carry_over = 0
+	# Setup
+	first_digit = None
+	current_digit = None
+	carry_over = 0
 
-    # Add
-    column_sum = head1.data + head2.data
-    if column_sum < 10:
-        new_digit = column_sum
-        carry_over = 0
-    else:
-        new_digit = column_sum
-        carry_over = 1
+	# Adding
+	while (head1 is not None) or (head2 is not None):
+		# If there's no head, then leading zero
+		if head1 is not None:
+			num1 = head1.data
+		else:
+			num1 = 0
 
-    # if new_head is None, make a new head
-    # else make a new node and attach as next digit
-    if first_digit is None:
-        first_digit = Node(new_digit)
-        current_digit = first_digit
+		if head2 is not None:
+			num2 = head2.data
+		else:
+			num2 = 0
 
 
-    # carry over adds 1 to the next value
+		# Add
+		column_sum = num1 + num2 + carry_over
 
-    # if there is a final carry
+		if column_sum < 10:
+			digit = column_sum
+			carry_over = 0
+		else:
+			digit = column_sum - 10
+			carry_over = 1
+
+		# If new_head is None, make a new head;
+		# else make a new node and attach as next digit
+		if first_digit is None:
+			first_digit = Node(digit)
+			current_digit = first_digit
+		else:
+			current_digit.next = Node(digit)
+			current_digit = current_digit.next
+
+		# Increment digits
+		if head1 is not None:
+			head1 = head1.next
+		if head2 is not None:
+			head2 = head2.next
 
 
-    return first_digit
+	# If there is a final carry
+	if carry_over > 0:
+		current_digit.next = Node(carry_over)
+
+
+	return first_digit
 
 
 # Test cases
 math_problems = [
-                (10, 52),   # 62
-                (99, 201),  # 300
-                (999, 1),   # 1000
-                (102, 899)  # 1002
-                ]
+				(10, 52),   # 62	-> 2 6
+				(99, 201),  # 300	-> 0 0 3
+				(999, 1),   # 1000	-> 0 0 0 1
+				(102, 899)  # 1001	-> 1 0 0 1
+				]
 
-value1 = 10
-value2 = 52
+for problem in math_problems:
+	value1, value2 = problem
+	head1 = convert_to_linked_list(value1)
+	head2 = convert_to_linked_list(value2)
+	total = add_linked_list_nums(head1, head2)
 
-head1 = convert_to_linked_list(value1)
-print('Value 1: {0}\nLinked List: '.format(value1), end=' ')
-print_linked_list(head1)
-print('\n')
-
-head2 = convert_to_linked_list(value2)
-print('Value 2: {0}\nLinked List: '.format(value2), end=' ')
-print_linked_list(head2)
-print('\n')
-
-total = add_linked_list_nums(head1, head2)
-print('Total in linked list: ', end=' ')
-print_linked_list(total)
-print()
+	print('Total in linked list format: ', end=' ')
+	print_linked_list(total)
+	print()
 
 
-# sum of different lengths and sum where final carry is the final digit
-# 99 + 201 = 300
-# 999 + 1 = 1000
-# 102 + 899 = 1002
+
+
+
+
+
+
 
 
 
