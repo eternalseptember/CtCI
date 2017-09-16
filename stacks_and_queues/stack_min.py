0a29783f3a740ea7fee0e9e84b0c511e5d65c5e1
KQ <eternal.september@gmail.com>
2017-09-16 18:38:00 -0400

    adjusting test case to follow answer key

--- stacks_and_queues/stack_min.py
+++ stacks_and_queues/stack_min.py
@@ -48,12 +48,17 @@ class Stack:
         print(self.min_item)
 
 
-# test
-values_to_add = [5, 6, 1, 2, 8, 0, 9]
+# Test
+# values_to_add = [5, 6, 1, 2, 8, 0, 9]
+values_to_add = [5, 6, 3, 7]
 
 new_stack = Stack()
 for value in values_to_add:
     new_stack.push(value)
     new_stack.print_min()
 
+
+for i in range(2):
+    new_stack.pop()
+    new_stack.print_min()
 
