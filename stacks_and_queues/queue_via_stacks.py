04b1c32517dabd5181264341037b852e9d9cdd2a
KQ <eternal.september@gmail.com>
2017-09-24 19:17:47 -0400

    minimizing the number of times things need to transfer between new and old stacks

--- stacks_and_queues/queue_via_stacks.py
+++ stacks_and_queues/queue_via_stacks.py
@@ -9,8 +9,11 @@ class MyQueue:
         self.oldest_stack = []
 
 
-    def __str__(self):
-        # this will probably be changed
+    def print_oldest_queue(self):
+        return str(self.oldest_stack)
+
+
+    def print_newest_queue(self):
         return str(self.newest_stack)
 
 
@@ -23,25 +26,25 @@ class MyQueue:
 
 
     def remove(self):
-        # move items to the oldest stack
-        while len(self.newest_stack) > 1:
-            item = self.newest_stack.pop()
-            self.oldest_stack.append(item)
+        self.move_to_oldest_stack()
 
-        # the last item is reached
-        remove_item = self.newest_stack.pop()
+        try:
+            return self.oldest_stack.pop()
+        except:
+            return None
 
-        # put the items back
-        while len(self.oldest_stack) > 0:
-            item = self.oldest_stack.pop()
-            self.newest_stack.append(item)
 
-        return remove_item
+    def move_to_oldest_stack(self):
+        if len(self.oldest_stack) == 0:
+            while len(self.newest_stack) > 0:
+                item = self.newest_stack.pop()
+                self.oldest_stack.append(item)
 
 
     def peek(self):
+        self.move_to_oldest_stack()
         try:
-            return self.newest_stack[0]
+            return self.oldest_stack[-1]
         except IndexError:
             return None
 
@@ -57,12 +60,12 @@ class MyQueue:
 test_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 test_queue = MyQueue()
 
+print('Adding items in queue:')
 for value in test_values:
     test_queue.add(value)
-
-print('Test queue: ', end=' ')
-print(test_queue)
+    print(test_queue.print_newest_queue())
 print()
+
 
 for i in range(len(test_values)):
     popped_item = test_queue.remove()
@@ -71,9 +74,11 @@ for i in range(len(test_values)):
     queue_front = test_queue.peek()
     print('front of queue: {0}'.format(queue_front))
 
-    print('remaining queue size: {0}'.format(test_queue.size()))
-    print('remaining queue: {0}'.format(test_queue))
     print('queue is empty? {0}'.format(test_queue.is_empty()))
+    print('remaining queue size: {0}'.format(test_queue.size()))
+    
+    # test printing queues
+
     print()
 
 
