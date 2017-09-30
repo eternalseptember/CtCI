8b0a81d60b6d79fdaff910f52115ca39dff75208
KQ <eternal.september@gmail.com>
2017-09-30 14:56:23 -0400

    modified animal_shelter to include ID number to track order

--- stacks_and_queues/animal_shelter.py
+++ stacks_and_queues/animal_shelter.py
@@ -13,47 +13,69 @@ LinkedList data structure.
 
 class AnimalShelter:
     def __init__(self):
-        self.master_queue = []
+        self.queue_num = 0
         self.cats_queue = []
         self.dogs_queue = []
 
 
-    def enqueue(self, animal_type, animal):
+    def enqueue(self, animal):
+        animal_type, name = (animal)
+        adoption_entry = (self.queue_num, name)
+
         if animal_type == "cat":
-            self.master_queue.append('cat')
-            self.cats_queue.append(animal)
+            self.cats_queue.append(adoption_entry)
         else:
-            self.master_queue.append('dog')
-            self.dogs_queue.append(animal)
+            self.dogs_queue.append(adoption_entry)
+        self.queue_num += 1
 
 
     def dequeueAny(self):
-        oldest = self.master_queue.pop(0)
-        if oldest == "cat":
-            return self.cats_queue.pop(0)
+        num_of_cats = len(self.cats_queue)
+        num_of_dogs = len(self.dogs_queue)
+
+        if (num_of_cats == 0) and (num_of_dogs != 0):
+            # if there are no cats
+            return self.dequeueDog()
+        elif (num_of_cats != 0) and (num_of_dogs == 0):
+            # if there are no dogs
+            return self.dequeueCat()
         else:
-            return self.dogs_queue.pop(0)
+            first_dog = self.dogs_queue[0]
+            dog_id, dog_name = (first_dog)
+            first_cat = self.cats_queue[0]
+            cat_id, cat_name = (first_cat)
+
+            if cat_id < dog_id:
+                return self.dequeueCat()
+            else:
+                return self.dequeueDog()
 
 
     def dequeueDog(self):
-        print('\tdequeue dog', end=' ')
-        print(self.master_queue.pop(0))
-        return self.dogs_queue.pop(0)
+        try:
+            adoptee = self.dogs_queue.pop(0)
+            id_num, name = (adoptee)
+            return name
+        except IndexError:
+            return None
 
 
     def dequeueCat(self):
-        print('\tdequeue cat', end=' ')
-        print(self.master_queue.pop(0))
-        return self.cats_queue.pop(0)
+        try:
+            adoptee = self.cats_queue.pop(0)
+            id_num, name = (adoptee)
+            return name
+        except IndexError:
+            return None
 
 
     def __str__(self):
-        return '{0}\n{1}\n{2}'.format(self.master_queue, self.cats_queue, self.dogs_queue)
+        return '{0}\n{1}'.format(self.cats_queue, self.dogs_queue)
 
 
 # Setting up test case
 shelter = AnimalShelter()
-values = [
+adoption_list = [
          ('cat', 'cat_1'),
          ('cat', 'cat_2'),
          ('dog', 'dog_1'),
@@ -66,17 +88,18 @@ values = [
          ('dog', 'dog_5'),
          ('dog', 'dog_6'),
          ]
-requests = ['any', 'dog', 'cat', 'dog', 'dog', 'dog', 'cat', 'any']
 
-for value in values:
-    animal, name = (value)
-    shelter.enqueue(animal, name)
+for animal in adoption_list:
+    shelter.enqueue(animal)
 
 print('Animals in the shelter:')
 print(shelter)
 print()
 
+
 # Dequeuing test
+requests = ['any', 'dog', 'cat', 'dog', 'dog', 'dog', 'cat', 'any']
+
 print('Adoption:')
 for request in requests:
     if request == 'dog':
@@ -89,5 +112,7 @@ for request in requests:
         item = shelter.dequeueAny()
         print('dequeue any: {0}'.format(item))
 
+
+# Results after dequeue requests
 print('\nRemaining animals in the shelter:')
 print(shelter)
