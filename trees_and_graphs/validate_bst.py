1368cacf3c32d4a6207592edf60b62464b71a03a
KQ <eternal.september@gmail.com>
2017-10-20 07:46:50 -0400

    implemented recursive solution

--- trees_and_graphs/validate_bst.py
+++ trees_and_graphs/validate_bst.py
@@ -10,6 +10,7 @@ class Node:
         self.right = right
 
 
+"""
 def check_bst(head):
     if head.left is not None:
         if head.left.data > head.data:
@@ -22,6 +23,25 @@ def check_bst(head):
             return False
         else:
             check_bst(head.right)
+    return True
+"""
+
+
+def check_bst(head, min=None, max=None):
+    # O(N) time, where N is the number of nodes in the tree.
+    # O(log N) space on a balanced tree.
+    # Up to O(log N) recursive calls on the stack up to the tree depth.
+    if head is None:
+        return True
+
+    if (((min is not None) and (head.data <= min)) or
+        ((max is not None) and (head.data > max))):
+        return False
+
+    if ((check_bst(head.left, min, head.data) is False) or
+        (check_bst(head.right, head.data, max) is False)):
+        return False
+
     return True
 
 
