f1f8293f97bf279079b548c2ad26f398b551fbd6
KQ <eternal.september@gmail.com>
2017-11-11 18:03:49 -0500

    setting up a file to test the solution

---
+++ trees_and_graphs/build_order_sol_tb.py
@@ -0,0 +1,15 @@
+# Testing the solution for the build order problem.
+
+
+from build_order_sol import *
+
+
+graph1 = Graph()
+nodes = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+for node in nodes:
+    graph1.get_or_create_node(node)
+
+
+
+
