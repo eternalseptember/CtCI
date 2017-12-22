8d03d9941a8621e7fc7c07e9383ae6a1bca844e9
KQ <eternal.september@gmail.com>
2017-12-22 05:17:05 -0500

    starting off the weave lists function

--- trees_and_graphs/bst_sequences_sol.py
+++ trees_and_graphs/bst_sequences_sol.py
@@ -15,4 +15,11 @@ def bst_sequences(head):
         return result
 
 
+def weave_lists(first_list, second_list, results, prefix):
+    if (len(first_list) == 0) or (len(second_list) == 0):
+        # result = clone the prefix list?
+        result = []
+        result.extend(first_list)
+        result.extend(second_list)
+        results.append(result)
 
