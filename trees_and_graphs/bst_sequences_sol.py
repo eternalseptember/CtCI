fdedf2088d7b356d6dacd38ab6bb2eb3d2fa6631
KQ <eternal.september@gmail.com>
2017-12-23 13:56:30 -0500

    commenting weave_first function

--- trees_and_graphs/bst_sequences_sol.py
+++ trees_and_graphs/bst_sequences_sol.py
@@ -16,10 +16,21 @@ def bst_sequences(head):
 
 
 def weave_lists(first_list, second_list, results, prefix):
+    # Weaves lists together in all possible ways. This algorithm works by
+    # removing the head from one list, recursing, and then doing the same
+    # thing with the other list.
+
+    # One list is empty. Add remainder to a cloned prefix and store result.
     if (len(first_list) == 0) or (len(second_list) == 0):
-        # result = clone the prefix list?
         result = []
         result.extend(first_list)
         result.extend(second_list)
         results.append(result)
+        return
+
+    # Recurse with head of first added to the prefix. Removing the head will
+    # damage first, so we'll need to put it back where we found it afterwards.
+    # remove_first function??
+    # add_last fiunction??
+    weave_lists(first, second, results, prefix)
 
