5ca1d20602ee5c643d953a8a83178df9eef75abd
KQ <eternal.september@gmail.com>
2017-12-26 14:21:22 -0500

    still figuring out weave_lists function

--- trees_and_graphs/bst_sequences_sol.py
+++ trees_and_graphs/bst_sequences_sol.py
@@ -7,6 +7,18 @@ class Node:
         self.left = left
         self.right = right
 
+    def add_first(self, item):
+        return None
+
+    def add_last(self, item):
+        return None
+
+    def remove_first(self):
+        return None
+
+    def remove_last(self):
+        return None
+
 
 def bst_sequences(head):
     result = []
@@ -25,7 +37,7 @@ def bst_sequences(head):
     for left_item in left_seq:
         for right_item in right_seq:
             weaved = []
-            weave_lists(left, right, weaved, prefix)
+            weave_lists(left_item, right_item, weaved, prefix)
             result.extend(weaved)
 
     return result
@@ -46,7 +58,14 @@ def weave_lists(first_list, second_list, results, prefix):
 
     # Recurse with head of first added to the prefix. Removing the head will
     # damage first, so we'll need to put it back where we found it afterwards.
-    # remove_first function??
-    # add_last fiunction??
-    weave_lists(first, second, results, prefix)
+    head_first = first_list.remove_first()
+    prefix.add_last(head_first)
+    weave_lists(first_list, second_list, results, prefix)
+    prefix.remove_last()
+    first_list.add_first(head_first)
+
+
+    # Do the same thing with second, damaging and then restoring the list.
+
+
 
