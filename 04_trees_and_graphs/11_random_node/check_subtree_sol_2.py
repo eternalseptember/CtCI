740dd3daafc61d30f5649e386dbe7af33de04bb4
KQ <eternal.september@gmail.com>
2018-01-28 02:47:46 -0500

    fleshing out solution

--- 04_trees_and_graphs/11_random_node/check_subtree_sol_2.py
+++ 04_trees_and_graphs/11_random_node/check_subtree_sol_2.py
@@ -10,6 +10,23 @@ class Tree:
 
     # size
 
+    def get_random_node(self):
+        if self.root is None:
+            return None
+
+        # more stuff here
+
+    def insert_in_order(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self.root.insert_in_order(value)
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+
 
 
 
