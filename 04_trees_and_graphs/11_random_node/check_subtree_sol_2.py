ebfd1d822723aec36d261ca561141f863be4473e
KQ <eternal.september@gmail.com>
2018-01-29 04:21:27 -0500

    working through tree_node

--- 04_trees_and_graphs/11_random_node/check_subtree_sol_2.py
+++ 04_trees_and_graphs/11_random_node/check_subtree_sol_2.py
@@ -24,8 +24,18 @@ class Tree:
 
 
 class TreeNode:
-    def __init__(self, value):
+    def __init__(self, value, left=None, right=None):
         self.value = value
+        self.left = left
+        self.right = right
+        self.size = 1
+
+    def get_ith_node(self, i):
+        if self.left is None:
+            left_size = 0
+        else:
+            self.left.size()
+        # skipping over left_size + 1 nodes, so subtract them
 
 
 
