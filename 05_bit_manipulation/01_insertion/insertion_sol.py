28fdd0be677af2bf9ba3bf3eeb62dc77928b2974
KQ <eternal.september@gmail.com>
2018-03-06 16:25:04 -0500

    the answer needs modification for python language

--- 05_bit_manipulation/01_insertion/insertion_sol.py
+++ 05_bit_manipulation/01_insertion/insertion_sol.py
@@ -8,11 +8,27 @@ def insert_bits(N, M, i, j):
     all_ones = ~0
     print(all_ones)  # -1??
 
-    # 1's before position j, then 0's. left = 11100000
+    # 1's before position j, then 0's.
+    # left = 11100000
     left = all_ones << (j + 1)
     print(left)
 
-    return None
+    # 1's after position i.
+    # right = 00000011
+    right = (1 << i) - 1
+    print(right)
+
+    # All 1's, except for 0's between i and j.
+    # mask = 11100011
+    mask = left | right
+    print(mask)
+
+    # Clear bits j through i, then put m in there.
+    n_cleared = N & mask  # Clear bits j through i.
+    m_shifted = M << i  # Move m into correct position.
+
+    # OR them.
+    return n_cleared | m_shifted
 
 
 
