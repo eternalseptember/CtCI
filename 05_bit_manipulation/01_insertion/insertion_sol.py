4a309f06f0626e660ed14b58b3317919cc424bf9
KQ <eternal.september@gmail.com>
2018-03-07 10:53:27 -0500

    trying to figure out how to set bit length

--- 05_bit_manipulation/01_insertion/insertion_sol.py
+++ 05_bit_manipulation/01_insertion/insertion_sol.py
@@ -5,7 +5,7 @@ def insert_bits(N, M, i, j):
     # Create a mask to clear bits i through j in n.
     # Example: i = 2, j = 4. Result should be 11100011.
 
-    all_ones = ~0
+    all_ones = 0xffffffff
     print(all_ones)  # -1??
 
     # 1's before position j, then 0's.
