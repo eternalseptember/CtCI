abdbab77f858d81b7bad9b31b607ffb6bbcb147d
KQ <eternal.september@gmail.com>
2018-03-09 17:43:28 -0500

    cleaned up the example, starting to figure out how binary works in python

--- 05_bit_manipulation/01_insertion/insertion_sol.py
+++ 05_bit_manipulation/01_insertion/insertion_sol.py
@@ -3,26 +3,21 @@
 
 def insert_bits(N, M, i, j):
     # Create a mask to clear bits i through j in n.
-    # Example: i = 2, j = 4. Result should be 11100011.
 
-    all_ones = 1
-    # print('{0:032b}'.format(all_ones))  # 32-bit numbers
-    format_print(all_ones)
+    all_ones = 0b111
+    format_print('all ones', all_ones)
 
     # 1's before position j, then 0's.
-    # left = 11100000
     left = all_ones << (j + 1)
-    format_print(left)
+    format_print('left mask', left)
 
     # 1's after position i.
-    # right = 00000011
     right = (1 << i) - 1
-    format_print(right)
+    format_print('right mask', right)
 
     # All 1's, except for 0's between i and j.
-    # mask = 11100011
     mask = left | right
-    format_print(mask)
+    format_print('mask', mask)
 
     # Clear bits j through i, then put m in there.
     n_cleared = N & mask  # Clear bits j through i.
@@ -30,12 +25,13 @@ def insert_bits(N, M, i, j):
 
     # OR them.
     result = n_cleared | m_shifted
-    format_print(result)
+    format_print('result', result)
     return result
 
 
-def format_print(item):
-    print('{:08b}'.format(item))
+def format_print(text, item):
+    # print('{0:032b}'.format(item))  # 32-bit numbers
+    print('{0:>12}: {1:08b}'.format(text, item))
 
 
 
--- 05_bit_manipulation/01_insertion/insertion_tb.py
+++ 05_bit_manipulation/01_insertion/insertion_tb.py
@@ -5,6 +5,22 @@
 from insertion_sol import *
 
 
+# Test case 0: Testing the mask from the solution's example.
+# left  = 11100000
+# right = 00000011
+# mask  = 11100011
+N = 10000000000
+M = 10011
+i = 2
+j = 4
+
+new_bit = insert_bits(N, M, i, j)
+# example works when all ones can be set to 0b111.
+print()
+
+
+
+
 # Test case 1
 # expected result: N = 10001001100
 N = 10000000000
