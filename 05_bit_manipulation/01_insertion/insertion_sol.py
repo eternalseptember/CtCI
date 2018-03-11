a335c6c1e8919f0cc5bce6d48e2d50f438d446f4
KQ <eternal.september@gmail.com>
2018-03-10 19:14:50 -0500

    still trying to figure out a python way to initalize "all ones"

--- 05_bit_manipulation/01_insertion/insertion_sol.py
+++ 05_bit_manipulation/01_insertion/insertion_sol.py
@@ -4,7 +4,8 @@
 def insert_bits(N, M, i, j):
     # Create a mask to clear bits i through j in n.
 
-    all_ones = 0b111
+    # since the problem states 32-bit numbers
+    all_ones = 0b1111111111111111111111111111111
     format_print('all ones', all_ones)
 
     # 1's before position j, then 0's.
@@ -31,7 +32,8 @@ def insert_bits(N, M, i, j):
 
 def format_print(text, item):
     # print('{0:032b}'.format(item))  # 32-bit numbers
-    print('{0:>12}: {1:08b}'.format(text, item))
+    # print('{0:>12}: {1:08b}'.format(text, item))
+    print('{0:>12}: {1:b}'.format(text, item))
 
 
 
