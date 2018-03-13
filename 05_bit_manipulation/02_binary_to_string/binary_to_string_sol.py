095d6f06e98f50fa6a43bce929a50a0c28765905
KQ <eternal.september@gmail.com>
2018-03-13 16:22:36 -0400

    setting the upper limit to the string

--- 05_bit_manipulation/02_binary_to_string/binary_to_string_sol.py
+++ 05_bit_manipulation/02_binary_to_string/binary_to_string_sol.py
@@ -4,6 +4,17 @@
 def string_to_binary(num):
     if (num >= 1) or (num <= 0):
         return "Error"
+
+
+    binary_str = '.'
+
+    while num > 0:
+        # setting a limit on length: 32 characters
+        if len(binary_str) >= 32:
+            return "Error"
+
+
+
     return None
 
 
