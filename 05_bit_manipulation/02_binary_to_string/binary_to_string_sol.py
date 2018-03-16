ccfac63718282faa1c085de7eafea4598e636bfe
KQ <eternal.september@gmail.com>
2018-03-16 08:50:30 -0400

    print debugging

--- 05_bit_manipulation/02_binary_to_string/binary_to_string_sol.py
+++ 05_bit_manipulation/02_binary_to_string/binary_to_string_sol.py
@@ -9,17 +9,23 @@ def string_to_binary(num):
     binary_str = '.'
 
     while num > 0:
+        print(len(binary_str))
+
         # setting a limit on length: 32 characters
         if len(binary_str) >= 32:
             return "Error 2"
 
         r = num * 2
+        print(r)
+
         if (r >= 1):
             binary_str += '1'
             num = r - 1
         else:
             binary_str += '0'
             num = r
+
+        print()
 
 
     return binary_str
