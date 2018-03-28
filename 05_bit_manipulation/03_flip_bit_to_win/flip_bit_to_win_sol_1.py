4e1fb35dfb0e838d0f298a12f048f1b4ce781f2b
KQ <eternal.september@gmail.com>
2018-03-28 14:32:07 -0400

    filling in get_alternating_sequences

--- 05_bit_manipulation/03_flip_bit_to_win/flip_bit_to_win_sol_1.py
+++ 05_bit_manipulation/03_flip_bit_to_win/flip_bit_to_win_sol_1.py
@@ -5,21 +5,36 @@ def longest_sequence_of_ones(num):
     if num == -1:
         # -1 in binary is a string of all ones.
         # This is already the longest sequence.
-        # return Integer.bytes * 8
-        # 32?
-        return "all ones"
-
+        # return Integer.bytes * 8 = 32?
+        return 32
 
     sequences = get_alternating_sequences(num)
     return find_longest_sequence(sequences)
 
 
 def get_alternating_sequences(num):
+    # Returns a list of the sixes of the sequences. The sequence starts
+    # off with the number of 0's (which might be 0) and then the
+    # alternates with the counts of each value.
     seq_list = []
+
+    searching_for = 0
+    counter = 0
+
+    # i in range Integer.BYTES
+    for i in range(32):
+        if ((num & 1) != searching_for):
+            seq_list.append(counter)
+            searching_for = num & 1  # Flip 1 to 0 or 0 to 1
+            counter = 0
+
+        counter += 1
+        # n >>>= 1
+
     return seq_list
 
 
-def find_longest_sequences(seq_list):
+def find_longest_sequence(seq_list):
     max_seq = 1
 
     return max_seq
