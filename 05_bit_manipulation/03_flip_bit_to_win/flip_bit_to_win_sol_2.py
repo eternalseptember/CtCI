d4955e1ee22842d5612ff8470844869005f71e23
KQ <eternal.september@gmail.com>
2018-04-07 16:37:18 -0400

    finished filling out optimized solution

--- 05_bit_manipulation/03_flip_bit_to_win/flip_bit_to_win_sol_2.py
+++ 05_bit_manipulation/03_flip_bit_to_win/flip_bit_to_win_sol_2.py
@@ -14,4 +14,24 @@ def longest_sequence_of_ones(num):
     previous_length = 0
     max_length = 1  # We can always have a sequence of at least one 1.
 
+    while (num != 0):
+        if ((num & 1) == 1):
+            # current bit is a 1
+            current_length += 1
+        elif ((num & 1) == 0):
+            # current bit is a 0
+            # if next bit is 0, update to 0
+            # if next bit is 1, update current_length
+            if ((num & 2) == 0):
+                previous_length = 0
+            else:
+                previous_length = current_length
+
+            current_length = 0
+
+        max_length = max(previous_length + current_length + 1, max_length)
+        num = num >> 1
+
+    return max_length
+
 
