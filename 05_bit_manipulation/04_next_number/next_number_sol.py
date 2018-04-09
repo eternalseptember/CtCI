84b94c30c7246c61a773bce564b1d5459978ae9f
KQ <eternal.september@gmail.com>
2018-04-09 19:08:01 -0400

    filling in get next function

--- 05_bit_manipulation/04_next_number/next_number_sol.py
+++ 05_bit_manipulation/04_next_number/next_number_sol.py
@@ -11,6 +11,20 @@ def next_sets_of_numbers(num):
 
 
 def get_next_smallest(num):
+    c = num
+    c0 = 0
+    c1 = 0
+
+    while (((c & 1) == 0) and (c != 0)):
+        c0 += 1
+        c >> 1
+
+    while ((c & 1) == 0):
+        c1 += 1
+        c >>= 1
+
+    #
+
     return None
 
 
