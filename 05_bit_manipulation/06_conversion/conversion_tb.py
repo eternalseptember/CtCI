0807e7cf7bfb75d5089bdada308cad80220134ba
KQ <eternal.september@gmail.com>
2018-04-19 13:32:54 -0400

    more test cases

--- 05_bit_manipulation/06_conversion/conversion_tb.py
+++ 05_bit_manipulation/06_conversion/conversion_tb.py
@@ -2,12 +2,27 @@ from conversion import *
 # from conversion_sol import *
 
 
-# Testing
+# Test case 1
 # Expected result: 2
 int_a = 29  # 11101
 int_b = 15  # 01111
 result = number_of_bits_to_flip(int_a, int_b)
 print(result)
 
+
+# Test case 2
+# Expected result = 0
+int_a = 0  # 0
+int_b = 0  # 0
+result = number_of_bits_to_flip(int_a, int_b)
+print(result)
+
+
+# Test case 3
+# Expected result = 1
+int_a = 1  # 1
+int_b = 0  # 0
+result = number_of_bits_to_flip(int_a, int_b)
+print(result)
 
 
