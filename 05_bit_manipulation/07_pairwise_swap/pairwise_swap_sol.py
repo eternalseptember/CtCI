5290dbaae351568de9e3524835d658fd5d9de200
KQ <eternal.september@gmail.com>
2018-04-25 17:11:49 -0400

    reformatting solution

--- 05_bit_manipulation/07_pairwise_swap/pairwise_swap_sol.py
+++ 05_bit_manipulation/07_pairwise_swap/pairwise_swap_sol.py
@@ -5,10 +5,11 @@ def pairwise_swap(num):
     # Mask odd bits with 0b101010...
     # Then shift them right by 1 to put them in even spots.
     # >>> is zero-fill right shift
-    # ((num & 0xaaaaaaaa) >>> 1)
-    # | 
-    # ((num & 0x55555555) << 1)
-    return None
+    odd_mask = (num & 0xaaaaaaaa) >> 1
+    even_mask = (num & 0x55555555) << 1
+    result = odd_mask | even_mask
+    # print(result)
+    return result
 
 
 
--- 05_bit_manipulation/07_pairwise_swap/pairwise_swap_tb.py
+++ 05_bit_manipulation/07_pairwise_swap/pairwise_swap_tb.py
@@ -2,15 +2,22 @@ from pairwise_swap import *
 # from pairwise_swap_sol import *
 
 
+def print_binary(num1, num2):
+    bin1 = bin(num1)[2:]
+    bin2 = bin(num2)[2:]
+    print('{0} <--> {1}'.format(bin1, bin2))
+
+
+
 # Testing
 # Test case 1: 101010 <-> 010101
 num = 42
 results = pairwise_swap(num)
-print(results)
+print_binary(num, results)
 
 
 # Test case 2: 111 or 0111 <-> 1011 
 num = 7
 results = pairwise_swap(num)
-print(results)
+print_binary(num, results)
 
