fe71a380dc2e52ba132f4932715541e221b0c4cb
KQ <eternal.september@gmail.com>
2018-04-27 16:49:03 -0400

    another test case

--- 05_bit_manipulation/07_pairwise_swap/pairwise_swap.py
+++ 05_bit_manipulation/07_pairwise_swap/pairwise_swap.py
@@ -8,7 +8,7 @@ and bit 3 are swapped, and so on).
 def pairwise_swap(num):
     # convert to integer
     bit_num = bin(num)[2:]
-    bit_num = [int(x) for x in bit_num]
+    bit_num = [int(bit) for bit in bit_num]
 
     # if the integer has an odd number of bits, put a zero bit in front
     if len(bit_num) % 2 == 1:
@@ -20,9 +20,7 @@ def pairwise_swap(num):
     for bit_1 in range(0, bit_len, 2):
         # LSB numbering; bit_1 is left and bit_0 is right
         bit_0 = bit_1 + 1
-
         bit_num[bit_1], bit_num[bit_0] = bit_num[bit_0], bit_num[bit_1]
-
 
     res_str = ''.join(str(bit) for bit in bit_num)
 
--- 05_bit_manipulation/07_pairwise_swap/pairwise_swap_tb.py
+++ 05_bit_manipulation/07_pairwise_swap/pairwise_swap_tb.py
@@ -1,12 +1,11 @@
-from pairwise_swap import *
-# from pairwise_swap_sol import *
+# from pairwise_swap import *
+from pairwise_swap_sol import *
 
 
 def print_binary(num1, num2):
     bin1 = bin(num1)[2:]
     bin2 = bin(num2)[2:]
     print('{0} <--> {1}'.format(bin1, bin2))
-
 
 
 # Testing
@@ -16,8 +15,15 @@ results = pairwise_swap(num)
 print_binary(num, results)
 
 
-# Test case 2: 111 or 0111 <-> 1011 
+# Test case 2: 111 or 0111 <-> 1011
 num = 7
 results = pairwise_swap(num)
 print_binary(num, results)
+
+
+# Test case 3: 10001 <-> 100010
+num = 17
+results = pairwise_swap(num)
+print_binary(num, results)
+
 
