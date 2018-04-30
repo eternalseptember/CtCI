2627105676d467f11d8e6292b35db7db2419a841
KQ <eternal.september@gmail.com>
2018-04-30 19:28:09 -0400

    filling in draw_screen function

--- 05_bit_manipulation/08_draw_line/draw_line_sol.py
+++ 05_bit_manipulation/08_draw_line/draw_line_sol.py
@@ -1,5 +1,17 @@
 # Solution
 
 
-def draw_screen(screen):
-    return None
+def draw_screen(screen, width, x1, x2, y):
+    start_offset = x1 % 8
+    first_full_byte = x1 // 8
+
+    if (start_offset != 0):
+        first_full_byte += 1
+
+    end_offset = x2 % 8
+    last_full_byte = x2 // 8
+
+    if (end_offset != 7):
+        last_full_byte -= 1
+
+
