2d72c822b2b96f8989ae77858165a7d917ad122f
KQ <eternal.september@gmail.com>
2018-05-01 16:24:25 -0400

    fleshing out draw_screen function

--- 05_bit_manipulation/08_draw_line/draw_line_sol.py
+++ 05_bit_manipulation/08_draw_line/draw_line_sol.py
@@ -14,4 +14,17 @@ def draw_screen(screen, width, x1, x2, y):
     if (end_offset != 7):
         last_full_byte -= 1
 
+    # Set full bytes
+    for b in range(first_full_byte, last_full_byte + 1):
+        # screen[(width // 8) * y + b] = (byte) 0xFF
+        screen[(width // 8) * y + b] = 0xFF
+
+    # Create masks for start and end of line
+    # start_mask = (byte) (0xFF >> start_offset)
+    start_mask = (0xFF >> start_offset)
+    # end_mask = (byte) ~(0xFF >> (end_offset + 1))
+    end_mask = ~(0xFF >> (end_offset + 1))
+
+    # Set start and end of line
+
 
