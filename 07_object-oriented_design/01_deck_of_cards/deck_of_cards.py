5a895742c49a57afeb44bab85e94675547541bcc
KQ <eternal.september@gmail.com>
2018-05-24 13:39:03 -0400

    fleshing out deck object

--- 07_object-oriented_design/01_deck_of_cards/deck_of_cards.py
+++ 07_object-oriented_design/01_deck_of_cards/deck_of_cards.py
@@ -8,10 +8,33 @@ class Deck():
         self.cards = []
 
 
+    def new_deck(self):
+        values = [2, 3, 4, 5, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+        suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades']
+
+
+    def shuffle(self):
+        num_of_cards = len(self.cards)
+
+
+    def draw_card(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        else:
+            return None
+
+    def __str__(self):
+        return str(self.cards)
+
+
+
 class Card():
     def __init__(self, suit, value):
         self.suit = suit
         self.value = str(value)
+
+    def __str__(self):
+        return '{0} of {1}'.format(self.value, self.suit)
 
 
 
