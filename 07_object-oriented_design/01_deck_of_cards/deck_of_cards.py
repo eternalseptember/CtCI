6d05b68ed7453f5c1b4c11f1913dc7644b851c96
KQ <eternal.september@gmail.com>
2018-05-25 14:11:02 -0400

    fleshing out deck object

--- 07_object-oriented_design/01_deck_of_cards/deck_of_cards.py
+++ 07_object-oriented_design/01_deck_of_cards/deck_of_cards.py
@@ -3,38 +3,80 @@ Design the data structure for a generic deck of cards.
 Explain how you would subclass the data structures to implement blackjack.
 """
 
+
+class Card():
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return '{0} of {1}'.format(self.rank, self.suit)
+
+
 class Deck():
     def __init__(self):
-        self.cards = []
+        self.cards = self.new_deck()
 
 
     def new_deck(self):
-        values = [2, 3, 4, 5, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
         suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades']
+
+        new_deck = []
+
+        for rank in ranks:
+            for suit in suits:
+                new_deck.append(Card(rank, suit))
+
+        return new_deck
 
 
     def shuffle(self):
+        from random import randrange
+
         num_of_cards = len(self.cards)
+        new_deck = []
+
+        for rand_num in randrange(num_of_cards):
+            new_deck.append(self.cards.pop(rand_num))
+            num_of_cards -= 1
+
+        self.cards = new_deck
 
 
-    def draw_card(self):
+    def deal(self):
         if len(self.cards) > 0:
             return self.cards.pop()
         else:
             return None
 
-    def __str__(self):
-        return str(self.cards)
-
-
-
-class Card():
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = str(value)
 
     def __str__(self):
-        return '{0} of {1}'.format(self.value, self.suit)
+        if len(self.cards) == 0:
+            return 'No cards in deck'
+
+        deck_list = ''
+
+        for card in self.cards:
+            deck_list += '{0}\n'.format(card)
+
+        return deck_list
+
+
+class Blackjack_Game():
+    def __init__(self):
+        new_deck = Deck().shuffle()
+
+    # aces are 1 or 11
+    # deal two cards to players
+    # anyone with ace and ten/suits wins
+
+    # players decide to draw or not
+    # when all players have stopped drawing cards
+    # the players with 21 in their hand wins
+    # else the players closest to 21 without going over wins
+
+
 
 
 
---
+++ 07_object-oriented_design/01_deck_of_cards/deck_of_cards_tb.py
@@ -0,0 +1,4 @@
+from deck_of_cards import *
+
+
+
