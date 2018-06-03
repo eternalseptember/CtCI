8cae36bbe9f9c1caa935eaa64d1717528082ba25
KQ <eternal.september@gmail.com>
2018-06-03 05:31:24 -0400

    setting up call center program

---
+++ 07_object-oriented_design/02_call_center/call_center.py
@@ -0,0 +1,17 @@
+"""
+Imagine you have a call center with three levels of employees: respondent,
+manager, and director. An incoming telephone call must be first allocated
+to a respondent who is free. If the respondent can't handle the call, he
+or she must escalate the call to a manager. If the manager is not free or
+not able to handle it, then the call should be escalated to a director.
+Design the classes and data structures for this program. Implement a method
+dispatchCall() which assigns a call to the first available employee.
+"""
+
+
+def dispatch_call():
+    return None
+
+
+
+
