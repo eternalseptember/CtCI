36f3592ce59d5ba120002a484ed6f9419ef9fc30
KQ <eternal.september@gmail.com>
2018-06-09 02:55:12 -0400

    setting up test for call center

--- 07_object-oriented_design/02_call_center/call_center_tb.py
+++ 07_object-oriented_design/02_call_center/call_center_tb.py
@@ -1,5 +1,20 @@
 from call_center import *
 
 
+call_center = Call_Center()
+
+# Add 10 respondents
+for i in range(10):
+    call_center.add_employee()
+
+
+# Add 4 managers
+for i in range(4):
+    call_center.add_employee(1)
+
+
+# Add 2 directors
+for i in range(2):
+    call_center.add_employee(2)
 
 
