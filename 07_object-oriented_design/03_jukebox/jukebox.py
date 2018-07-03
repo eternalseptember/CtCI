3cf74a7445e9e6568a78c3d3451166e340a87fd6
KQ <eternal.september@gmail.com>
2018-07-03 19:18:45 -0400

    setting up jukebox problem

---
+++ 07_object-oriented_design/03_jukebox/jukebox.py
@@ -0,0 +1,10 @@
+"""
+Design a musical jukebo using object-oriented principles.
+"""
+
+
+class Jukebox():
+    def __init__(self):
+        self.music_list = []
+
+
---
+++ 07_object-oriented_design/03_jukebox/jukebox_tb.py
@@ -0,0 +1,4 @@
+from jukebox import *
+
+
+
