d5d250322774587eeb4a148a08c85e0c4f91352f
KQ <eternal.september@gmail.com>
2018-07-17 01:32:49 -0400

    setting up parking lot problem

---
+++ 07_object-oriented_design/04_parking_lot/parking_lot.py
@@ -0,0 +1,9 @@
+# Design a parking lot using object-oriented principles.
+
+
+class Parking_Lot():
+    def __init__(self, num_spots):
+        self.num_spots = num_spots
+
+
+
---
+++ 07_object-oriented_design/04_parking_lot/parking_lot_tb.py
@@ -0,0 +1,3 @@
+from parking_lot import *
+
+
