92dbdfd3e01f76b1e4ae286e19d35ebc66d55b0a
KQ <eternal.september@gmail.com>
2018-07-28 07:30:53 -0400

    car leaves

--- 07_object-oriented_design/04_parking_lot/parking_lot_tb.py
+++ 07_object-oriented_design/04_parking_lot/parking_lot_tb.py
@@ -10,10 +10,13 @@ car3 = Car('756')
 parking_lot_1.car_park(5, car1)
 parking_lot_1.car_park(8, car2)
 
-print(parking_lot_1)
-
 # trying to park in an occupied spot
 parking_lot_1.car_park(5, car3)
-print(parking_lot_1)
 
+# car3 finds another spot
+parking_lot_1.car_park(2, car3)
+
+# car1 leaves the lot
+parking_lot_1.car_leave(5)
+print(parking_lot_1)
 
