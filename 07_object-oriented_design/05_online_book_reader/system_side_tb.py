7d2bddc902dafb9e8d6ec05f2002972ca5bb8764
KQ <eternal.september@gmail.com>
2018-08-10 18:34:58 -0400

    more examples to test adding books to system

--- 07_object-oriented_design/05_online_book_reader/system_side_tb.py
+++ 07_object-oriented_design/05_online_book_reader/system_side_tb.py
@@ -4,14 +4,20 @@ from online_book_reader import *
 # Books
 hp1 = ('J. K. Rowling', 'Harry Potter and the Philosopher\'s Stone', 'Scholastic', 1998)
 hp2 = ('J. K. Rowling', 'Harry Potter and the Chamber of Secrets', 'Scholastic', 1998)
-lotr1 = ('J. R. R. Tolkien', 'The Lord of the Rings', 'Allen & Unwin', 1954)
+hp3 = ('J. K. Rowling', 'Harry Potter and the Prisoner of Azkaban', 'Scholastic', 1999)
+lotr = ('J. R. R. Tolkien', 'The Lord of the Rings', 'Allen & Unwin', 1954)
 asoiaf1 = ('George R. R. Martin', 'A Game of Thrones', 'Bantam Books', 1996)
+asoiaf2 = ('George R. R. Martin', 'A Clash of Kings', 'Bantam Books', 1998)
+asoiaf3 = ('George R. R. Martin', 'A Storm of Swords', 'Bantam Books', 2000)
 
 
+books_to_add = [hp1, hp2, hp3, lotr, asoiaf1, asoiaf2, asoiaf3]
 
 
 service_library = Service_Library()
-service_library.add_book(hp1)
+
+for book in books_to_add:
+    service_library.add_book(book)
 
 print(service_library)
 
