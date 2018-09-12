6f63fd6a6c483ea4b6a31c6baee669a57ad83f1b
KQ <eternal.september@gmail.com>
2018-09-12 11:27:15 -0400

    setting up jigsaw puzzle problem

---
+++ 07_object-oriented_design/06_jigsaw/jigsaw.py
@@ -0,0 +1,13 @@
+"""
+Implement an NxN jigsaw puzzle. Design the data structures and explain
+an algorithm to solve the puzzle. You can assume that you have a fitsWith
+method which, when passed two puzzle edges, returns True if the two edges
+belong together.
+"""
+
+
+def fits_with(edge_1, edge_2):
+    # return True if the two edges belong together
+    return None
+
+
