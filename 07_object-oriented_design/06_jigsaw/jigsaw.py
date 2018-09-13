1b738a74fba014063cb3473bdaff5288fc37d71c
KQ <eternal.september@gmail.com>
2018-09-13 07:54:58 -0400

    working on puzzle piece problem

--- 07_object-oriented_design/06_jigsaw/jigsaw.py
+++ 07_object-oriented_design/06_jigsaw/jigsaw.py
@@ -6,8 +6,23 @@ belong together.
 """
 
 
-def fits_with(edge_1, edge_2):
-    # return True if the two edges belong together
-    return None
+class Puzzle():
+    def __init__(self, puzzle_size):
+        self.puzzle_size = puzzle_size  # N
+
+        # how to record the position of a piece?
+        # should it be stored by row and location?
+
+    def fits_with(edge_1, edge_2):
+        # return True if the two edges belong together
+        return None
+
+
+class Puzzle_Piece():
+    def __init__(self, num_of_edges):
+        self.num_of_edges = num_of_edges
+
+
+
 
 
