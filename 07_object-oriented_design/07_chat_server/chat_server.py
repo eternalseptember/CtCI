c92db81e755d7a0a7d759dabffcbf206ff977d9f
KQ <eternal.september@gmail.com>
2018-10-18 05:55:27 -0400

    fleshing out big picture uses

--- 07_object-oriented_design/07_chat_server/chat_server.py
+++ 07_object-oriented_design/07_chat_server/chat_server.py
@@ -16,12 +16,30 @@ class Chat():
     def __init__(self, participants):
         # group chat?
         self.participants = participants  # list of users in the chat
-        self.log = ''  # write this to a log
+        # write chat log to a text file
+        # location of chat logs?
+
+
+    def begin_chat(self, user_1, user_2):
+        # user_1 is the person starting the chat with user_2
+        # open some file streams?
+        print()
+
+
+    def send(self, user_1, user_2, message):
+        # user_1 sends the message to user_2
+        # write the message to the text file
+        print()
 
 
 class Chat_Server():
     def __init__(self):
+        self.users = []  # username and status?
         self.log = ''
+
+
+    def add_user(self, username):
+        self.users.append(username)
 
 
 
