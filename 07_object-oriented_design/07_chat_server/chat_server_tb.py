5ba8ed0de7de0ea35066fb925ff7de9cde4b64a8
KQ <eternal.september@gmail.com>
2018-11-03 21:27:33 -0400

    more testing

--- 07_object-oriented_design/07_chat_server/chat_server_tb.py
+++ 07_object-oriented_design/07_chat_server/chat_server_tb.py
@@ -25,6 +25,8 @@ user_1.send_contact_request(chat_server, 'test_user_2')
 user_2.check_contact_requests(chat_server)
 user_1.send_contact_request(chat_server, 'test_user_3')
 user_3.check_contact_requests(chat_server)
+user_2.send_contact_request(chat_server, 'test_user_3')
+user_3.check_contact_requests(chat_server)
 
 print_users()
 
