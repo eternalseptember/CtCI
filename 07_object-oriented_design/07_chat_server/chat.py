a268a540c364f5afd0d5875d7719db94641385df
KQ <eternal.september@gmail.com>
2018-11-08 16:09:20 -0500

    working on chat

--- 07_object-oriented_design/07_chat_server/chat.py
+++ 07_object-oriented_design/07_chat_server/chat.py
@@ -2,10 +2,16 @@
 
 
 class Chat():
-    def __init__(self, participants):
-        self.participants = participants  # list of users in the chat
+    def __init__(self, participant):
+        # Participants should be a list of users in the chat,
+        # but first, assumes that it begins as a two-party chat.
+        self.participants = [participant]
         # write chat log to a text file
         # location of chat logs?
+
+
+    def invite_to_chat(self, participant):
+        self.participants.append(participant)
 
 
 
