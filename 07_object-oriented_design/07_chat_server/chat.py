22bfed514b2f5bfd37a0d6aeffb4b135b0fc0518
KQ <eternal.september@gmail.com>
2018-11-11 21:01:51 -0500

    still working on chat log

--- 07_object-oriented_design/07_chat_server/chat.py
+++ 07_object-oriented_design/07_chat_server/chat.py
@@ -6,6 +6,8 @@ class Chat():
         # Participants should be a list of users in the chat.
         self.participants = participants
         self.chat_id = 0  # should be set by the server to find chat logs
+        self.chat_log = self.start_chat_log
+
         # write chat log to a text file
         # location of chat logs?
 
@@ -18,6 +20,9 @@ class Chat():
     def start_chat_log(self):
         # Pretend that the chat log is like the chat window.
         print('write to chat_logs')
+
+        # placeholder - should return file address
+        return self.chat_id
 
 
 
