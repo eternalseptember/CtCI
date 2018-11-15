32271bb48278cc9a2eb0dc3b96f5c9c38f7b2588
KQ <eternal.september@gmail.com>
2018-11-15 14:56:09 -0500

    writing to a chat log folder

--- 07_object-oriented_design/07_chat_server/chat.py
+++ 07_object-oriented_design/07_chat_server/chat.py
@@ -2,10 +2,10 @@
 
 
 class Chat():
-    def __init__(self, participants):
+    def __init__(self, participants, chat_id):
         # Participants should be a list of users in the chat.
         self.participants = participants
-        self.chat_id = 0  # should be set by the server to find chat logs
+        self.chat_id = chat_id  # should be set by the server to find chat logs
         self.chat_log = self.start_chat_log()
 
         # write chat log to a text file
@@ -21,8 +21,9 @@ class Chat():
         # Pretend that the chat log is like the chat window.
 
         # placeholder - should return file address
-        file_name = str(self.chat_id) + ".txt"
-        print('write to chat log: {0}'.format(file_name))
+        file_name = 'chat_logs/' + str(self.chat_id) + '.txt'
+        chat_log = open(file_name, 'w')
+        chat_log.write('write to chat log: {0}'.format(file_name))
 
         return file_name
 
