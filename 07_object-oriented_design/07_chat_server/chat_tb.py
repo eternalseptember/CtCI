4e2a1598c6856229057279546ce9ec49915d337f
KQ <eternal.september@gmail.com>
2018-11-18 13:05:05 -0500

    testing message sending

--- 07_object-oriented_design/07_chat_server/chat_tb.py
+++ 07_object-oriented_design/07_chat_server/chat_tb.py
@@ -9,7 +9,7 @@ print(test_chat_1)
 participants_2 = ['test_user_3', 'test_user_4']
 test_chat_2 = Chat(participants_2, 2)
 print(test_chat_2)
-
+test_chat_2.send_message('test_user_3', 'test message')
 
 
 
