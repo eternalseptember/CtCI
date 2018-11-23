6c6e64a8282f9fd0232a12501cc58fbe6e8d7477
KQ <eternal.september@gmail.com>
2018-11-23 15:56:55 -0500

    fixed hashing issue

--- 07_object-oriented_design/07_chat_server/chat_server.py
+++ 07_object-oriented_design/07_chat_server/chat_server.py
@@ -92,6 +92,7 @@ class Chat_Server():
     def begin_chat(self, participants):
         # Look for an existing chat between these participants.
         participants.sort()
+        participants = tuple(participants)
 
         # If previous chat log is not available, start new chat log.
         if participants not in self.chat_id_list:
