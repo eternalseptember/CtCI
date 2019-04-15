eef7dd12f69480156ef6fad262b6b583c1fd9951
KQ <eternal.september@gmail.com>
2019-04-15 11:37:08 -0400

    compacting the check direction functions

--- 07_object-oriented_design/08_othello/o_check.py
+++ 07_object-oriented_design/08_othello/o_check.py
@@ -11,23 +11,35 @@ def is_valid(self, row, col, color_placed):
         #
         # Checking in every direction isn't really necessary.
         #
-        flip_right = self.check_right(row, col, color_placed)
-        flip_left = self.check_left(row, col, color_placed)
+        flip_left = self.check_row(row, col, color_placed, "W")
+        flip_right = self.check_row(row, col, color_placed, "E")
         flip_below = self.check_below(row, col, color_placed)
         flip_above = self.check_above(row, col, color_placed)
 
         return flip_left or flip_right or flip_above or flip_below
 
 
-def check_left(self, row, col, color_placed):
-    # If the piece was placed on the left edge...
-    if col <= 0:
-        return False
+def check_row(self, row, col, color_placed, direction):
+    # Direction: "W" (left) or "E" (right)
+    if direction == "W":  # Left
+        if col <= 0:  # Piece was placed on left edge.
+            return False
 
-    # If the piece was placed anywhere else...
+        start = col-1
+        stop = -1
+        step = -1
+
+    elif direction == "E":  # Right
+        if col >= 7:  # Piece was placed on right edge.
+            return False
+
+        start = col+1
+        stop = 8
+        step = 1
+
     opp_color = False
 
-    for position in range(col-1, -1, -1):
+    for position in range(start, stop, step):
         piece = self.board[row][position]
 
         if piece is None:
@@ -45,46 +57,28 @@ def check_left(self, row, col, color_placed):
     return False
 
 
-def check_right(self, row, col, color_placed):
-    # If the piece was placed on the right edge...
-    if col >= 7:
-        return False
-
-    # If the piece was placed anywhere else...
-    opp_color = False
-
-    for position in range(col+1, 8):
-        piece = self.board[row][position]
-
-        if piece is None:
-            return False
-        else:
-            if str(piece) == color_placed:
-                continue
-            else:
-                # Something can be flipped
-                return True
-
-    return False
-
-
 def check_above(self, row, col, color_placed):
     # If a piece was placed on the top edge...
     if col <= 0:
         return False
 
     # If the piece was placed anywhere else...
+    opp_color = False
+
     for position in range(row-1, -1, -1):
         piece = self.board[position][col]
-        # check the decrement...
+
         if piece is None:
             return False
         else:
             if str(piece) == color_placed:
-                continue
+                if opp_color:
+                    return True
+                else:
+                    return False
             else:
-                # something can be flipped
-                return True
+                if opp_color is False:
+                    opp_color = True
 
     return False
 
@@ -95,16 +89,22 @@ def check_below(self, row, col, color_placed):
         return False
 
     # If the piece was placed anywhere else...
+    opp_color = False
+
     for position in range(row+1, 8):
         piece = self.board[position][col]
+
         if piece is None:
             return False
         else:
             if str(piece) == color_placed:
-                continue
+                if opp_color:
+                    return True
+                else:
+                    return False
             else:
-                # Something can be flipped
-                return True
+                if opp_color is False:
+                    opp_color = True
 
     return False
 
