c3b0b12018767842579d7f047a3b5070ccf7ed01
KQ <eternal.september@gmail.com>
2019-05-22 13:27:31 -0400

    working out a game until the end state

--- 07_object-oriented_design/08_othello/test_game_2.py
+++ 07_object-oriented_design/08_othello/test_game_2.py
@@ -8,24 +8,33 @@ def place_piece(self, player, row, col):
     self.move_checks.clear()
 
 
-#
 # Black goes first.
-#
 active_player = self.players[0]
 place_piece(self, active_player, 2, 3)
 
-#
 # White's turn.
-#
 active_player = self.players[1]
 place_piece(self, active_player, 4, 2)
 
-
-#
 # Black's turn
-#
 active_player = self.players[0]
+place_piece(self, active_player, 5, 4)
 
+# White's turn.
+active_player = self.players[1]
+place_piece(self, active_player, 4, 5)
+
+# Black's turn
+active_player = self.players[0]
+place_piece(self, active_player, 5, 3)
+
+# White's turn.
+active_player = self.players[1]
+place_piece(self, active_player, 6, 4)
+
+# Black's turn
+active_player = self.players[0]
+place_piece(self, active_player, 5, 5)
 
 
 
