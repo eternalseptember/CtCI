087bafbe6f4c3b566cddb2f66fbc7e5a7f477d22
KQ <eternal.september@gmail.com>
2019-05-25 14:10:45 -0400

    the game continues

--- 07_object-oriented_design/08_othello/test_game_2.py
+++ 07_object-oriented_design/08_othello/test_game_2.py
@@ -56,11 +56,17 @@ place_piece(self, active_player, 4, 6)
 active_player = self.players[1]
 place_piece(self, active_player, 4, 7)
 
-# Black has to pass turn?
+# Black's turn
+active_player = self.players[0]
+place_piece(self, active_player, 3, 5)
 
+# White's turn.
+active_player = self.players[1]
+place_piece(self, active_player, 5, 2)
 
-
-
+# Black's turn
+active_player = self.players[0]
+place_piece(self, active_player, 5, 1)
 
 
 
