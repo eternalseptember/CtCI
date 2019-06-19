8cf68749d52d7ffb4cdab1a47d1dd11c51620f95
KQ <eternal.september@gmail.com>
2019-06-19 17:39:56 -0400

    still setting up the game

--- 07_object-oriented_design/08_othello/test_game_2b.py
+++ 07_object-oriented_design/08_othello/test_game_2b.py
@@ -18,6 +18,12 @@ othello_game.board = [[None, 'B', 'B', 'B', 'B', 'B', None, None],
                       ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'], 
                       [None, 'B', 'B', 'B', 'B', 'B', None, None]]
 
+# white's turn
+othello_game.pieces_played = 52
+othello_game.playable_spots = [(3, 6), (3, 7), (7, 6), (7, 7), (3, 0),
+                               (7, 0), (2, 6), (0, 6), (2, 0), (0, 0),
+                               (0, 7), (2, 7)]
+
 othello_game.begin_game(True)
 
 
