000a027b86288371c9385ef8a59c222edd773b17
KQ <eternal.september@gmail.com>
2019-06-24 15:59:18 -0400

    rotate method

--- 07_object-oriented_design/09_circular_array/cirular_array.py
+++ 07_object-oriented_design/09_circular_array/cirular_array.py
@@ -5,8 +5,14 @@ type (also called a template), and should support iteration via the standard
 for (Obj o : circularArray) notation.
 """
 
+# The rotate() method should be able to run in O(1) time.
+
 
 class CircularArray:
     def __init__(self, items):
         self.items = items
+
+
+    def rotate(self):
+        return None
 
