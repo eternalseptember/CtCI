df16821d58984c402c60ae89fcaaa2e1d792f8e9
KQ <eternal.september@gmail.com>
2019-06-26 10:21:08 -0400

    learning about circular array

--- 07_object-oriented_design/09_circular_array/cirular_array.py
+++ 07_object-oriented_design/09_circular_array/cirular_array.py
@@ -1,6 +1,6 @@
 """
 Implement a CircularArray class that supports an array-like data structure
-which can be efficiently rotated. IF possible, the class should use a generic
+which can be efficiently rotated. If possible, the class should use a generic
 type (also called a template), and should support iteration via the standard
 for (Obj o : circularArray) notation.
 """
@@ -9,8 +9,13 @@ for (Obj o : circularArray) notation.
 
 
 class CircularArray:
-    def __init__(self, items):
-        self.items = items
+    def __init__(self, size):
+        self.queue = [None for item in range(size)]
+
+
+    def add(self, item):
+        # figure out where heads and tails are
+        return None
 
 
     def rotate(self):
