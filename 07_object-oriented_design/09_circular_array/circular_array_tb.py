5c1caa38c4dae317d53387998594ef6dcba8bf2d
KQ <eternal.september@gmail.com>
2019-07-20 15:33:38 -0400

    testing

--- 07_object-oriented_design/09_circular_array/circular_array_tb.py
+++ 07_object-oriented_design/09_circular_array/circular_array_tb.py
@@ -10,4 +10,11 @@ print(queue_1)
 queue_1.clear()
 print(queue_1)
 
-
+queue_1.enqueue(1)
+queue_1.enqueue(5)
+queue_1.enqueue(9)
+queue_1.enqueue(2)
+queue_1.enqueue(6)
+queue_1.enqueue(5)
+queue_1.enqueue(3)
+# one left
