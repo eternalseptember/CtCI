44fb9dc4ee66c98d263b2b663c167f07b5e4620c
KQ <eternal.september@gmail.com>
2019-07-24 10:26:21 -0400

    simple print function

--- 07_object-oriented_design/09_circular_array/circular_array_tb.py
+++ 07_object-oriented_design/09_circular_array/circular_array_tb.py
@@ -1,6 +1,13 @@
 from circular_array import *
 
 
+def print_queue_status(queue):
+    q_full = queue.is_full()
+    q_empty = queue.is_empty()
+    print('Queue full? {0}'.format(q_full))
+    print('Queue empty? {0}'.format(q_empty))
+
+
 queue_1 = CircularArray(10)
 queue_1.enqueue(3)
 queue_1.enqueue(1)
@@ -22,10 +29,14 @@ queue_1.enqueue(8)
 print(queue_1)
 
 
-q_full = queue_1.is_full()
-q_empty = queue_1.is_empty()
-print('Queue full? {0}'.format(q_full))
-print('Queue empty? {0}'.format(q_empty))
+# should be neither full nor empty
+print_queue_status(queue_1)
+
+# should be full
+queue_1.enqueue(9)
+print_queue_status(queue_1)
+
+# should be empty
 queue_1.clear()
 print(queue_1)
 
