21b60a09e7f3fbb59c7326c6f2ad5c6f113903a9
KQ <eternal.september@gmail.com>
2019-08-14 11:01:28 -0400

    wraps around

--- 07_object-oriented_design/09_circular_array/circular_array_tb.py
+++ 07_object-oriented_design/09_circular_array/circular_array_tb.py
@@ -58,42 +58,15 @@ queue_2.enqueue(7)
 queue_2.enqueue(8)
 print_queue_status(queue_2)
 
+print('=== Wraps around. ===')
+queue_2.enqueue(9)
+print_queue_status(queue_2)
+
 
 # Full first, then dequeue...
-
-"""
-item = queue_2.dequeue()
-print('item pulled: {0}'.format(item))
-print(queue_2)
-
-# pull remaining two items and then add. where is the new item?
-queue_2.dequeue()
-queue_2.dequeue()
-queue_2.enqueue('A')
-print(queue_2)
+# what happens when queue is full?
 
 
-# should wrap around
-queue_2.enqueue('B')
-queue_2.enqueue('C')
-queue_2.enqueue('D')
-queue_2.enqueue('E')
-queue_2.enqueue('F')
-queue_2.enqueue('G')
-queue_2.enqueue('H')
-print('queue should wrap around')
-print(queue_2)
-
-# what happens when full?
-print('what happens when queue is full?')
-queue_2.enqueue('I')
-queue_2.enqueue('J')  # not adding?
-print(queue_2)
-
-#
-queue_2.enqueue('K')
-print(queue_2)
-"""
 
 
 
