b6fb8a78f2ae5d60ab43f190c15b7f0f5c33d00a
KQ <eternal.september@gmail.com>
2019-08-23 12:40:08 -0400

    answer key's get and set function

--- 07_object-oriented_design/09_circular_array/circular_array_sol.py
+++ 07_object-oriented_design/09_circular_array/circular_array_sol.py
@@ -7,14 +7,24 @@ class CircularArray():
         self.items = [None for item in range(size)]
         self.head = 0
 
+
     def convert(self, index):
         if (index < 0):
             index += self.size
 
         return (self.head + index) % self.size
 
+
     def rotate(self, index):
         self.head = self.convert(index)
+
+
+    def get(self):
+        return None
+
+
+    def set(self, item):
+        return None
 
 
 
