8e9a2a01a3e4db3f354cf7eae77d68563d6be1d6
KQ <eternal.september@gmail.com>
2019-09-05 09:49:12 -0400

    properties of cells

--- 07_object-oriented_design/10_minesweeper/minesweeper.py
+++ 07_object-oriented_design/10_minesweeper/minesweeper.py
@@ -1,28 +1,37 @@
 """
 Design and implement a text-based Minesweeper game. Minesweeper is the classic
-single-player computer game where an NxN grid has Bmines (or bombs) hidden
+single-player computer game where an NxN grid has B mines (or mines) hidden
 across the grid. The remaining cells are either blank or have a number behind
-them. The numbers reflect the number of bombs in the surrounding eight cells.
-The user then uncovers a cell. If it is a bomb, the player loses. If it is a
+them. The numbers reflect the number of mines in the surrounding eight cells.
+The user then uncovers a cell. If it is a mine, the player loses. If it is a
 number, the number is exposed. If it is a blank cell, this cell and all
 adjacent blank cells (up to and including the surrounding numeric cells) are
-exposed. The player wins when all non-bomb cells are exposed. The player can
-also flag certain places as potential bombs. This doesn't affect game play,
+exposed. The player wins when all non-mine cells are exposed. The player can
+also flag certain places as potential mines. This doesn't affect game play,
 other than to block the user from accidentally clicking a cell that is thought
-to have a bomb.
+to have a mine.
 """
 
 
+
+
+
 class Cell():
-    def __init__(self, is_bomb, num_of_neighbor_bombs):
-        self.bomb = is_bomb
-        self.num_of_neighbor_bombs = num_of_neighbor_bombs
-        self.marked = False
+    def __init__(self, is_mine, num_of_neighbor_mines):
+        self.mine = is_mine
+        self.num_of_neighbor_mines = num_of_neighbor_mines
+        self.flagged = False
         self.revealed = False
 
 
-    def is_bomb(self):
-        return self.bomb
+    def is_mine(self):
+        return self.mine
+
+    def flag(self):
+        self.flagged = True
+
+    def unflag(self):
+        self.flagged = False
 
 
 class Minesweeper():
@@ -30,10 +39,12 @@ class Minesweeper():
         self.size = size
         self.num_of_mines = num_of_mines
         self.board = self.init_game_board(size)
+        self.cells_revealed = 0
 
 
     def init_game_board(self, size):
         # len(board) is row; len(board[0]) is col
+        # Creates a blank board.
         init_board = [
             [None for col in range(size)] for row in range(size)
             ]
@@ -53,10 +64,10 @@ class Minesweeper():
 
 
     def choose_cell(self, row, col):
-        # should reveal whether the cell is a bomb or not
+        # should reveal whether the cell is a mine or not
         cell = self.board[row][col]
-        if cell.bomb:
-            print('bomb')
+        if cell.mine:
+            print('mine')
         return None
 
 
