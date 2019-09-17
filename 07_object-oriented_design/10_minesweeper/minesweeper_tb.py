e5712742f469cb0c7671a18ddc5232a6bcab4220
KQ <eternal.september@gmail.com>
2019-09-17 12:37:31 -0400

    testing list neighboring cells function

--- 07_object-oriented_design/10_minesweeper/minesweeper_tb.py
+++ 07_object-oriented_design/10_minesweeper/minesweeper_tb.py
@@ -5,5 +5,14 @@ from minesweeper import *
 
 game = Minesweeper(10, 10)
 game.print_board()
-game.choose_cell(4, 4)
+# game.choose_cell(4, 4)
+
+
+# Testing list neighboring cells
+
+# Somewhere in the middle
+neigh_cells = game.list_neighboring_cells(4, 4)
+print(neigh_cells)
+
+# Corners
 
