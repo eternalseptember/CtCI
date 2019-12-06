ea9ead6371ed3ee6a953373089bd6591272f62f1
KQ <eternal.september@gmail.com>
2019-12-06 12:11:55 -0500

    starting up the hash table problem

---
+++ 07_object-oriented_design/12_hash_table/hash_table.py
@@ -0,0 +1,13 @@
+"""
+Design and implement a hash table which uses chaining (linked lists) to handle
+collisions.
+"""
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+
+
+
+
