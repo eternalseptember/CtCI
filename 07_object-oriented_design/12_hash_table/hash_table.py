9e392eb6e83df5073fb61c4c833cd8e793a82a4d
KQ <eternal.september@gmail.com>
2019-12-07 15:51:12 -0500

    getting refresher on hash table

--- 07_object-oriented_design/12_hash_table/hash_table.py
+++ 07_object-oriented_design/12_hash_table/hash_table.py
@@ -4,6 +4,15 @@ collisions.
 """
 
 
+class Hash_Table():
+    def __init__(self):
+        self.linked_list = []
+
+    def get_hash_code(self):
+        # computation
+        print()
+
+
 class Node():
     def __init__(self, data):
         self.data = data
