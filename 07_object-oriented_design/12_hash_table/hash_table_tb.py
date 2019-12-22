2159f2b1308a61fda7769bd7f526cfa3d27549c0
KQ <eternal.september@gmail.com>
2019-12-21 19:38:54 -0500

    test examples with collisions

--- 07_object-oriented_design/12_hash_table/hash_table_tb.py
+++ 07_object-oriented_design/12_hash_table/hash_table_tb.py
@@ -1,7 +1,7 @@
 from hash_table import *
 
 # The first name is key, and the value is the last name.
-test_set = [
+test_set_1 = [
     ('Jan', 'Graves'),
     ('Tim', 'King'),
     ('Mia', 'Mann'),
@@ -15,9 +15,18 @@ test_set = [
     ('Zoe', 'Rice')
     ]
 
+test_set_2 = [
+    ('Mia', 'Mann'),
+    ('Tim', 'King'),
+    ('Bea', 'Stone'),
+    ('Zoe', 'Rice'),
+    ('Sue', 'Steele'),  # conflicts with Mia
+    ('Len', 'Bell')  # conflicts with Tim
+    ]
+
 test_hash = Hash_Table(11)
 
-for item in test_set:
+for item in test_set_1:
     key, value = (item)
     test_hash.insert(key, value)
 
