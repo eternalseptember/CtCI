5a951b746106c8fc6bc8e9e5f70a1d29e3605584
KQ <eternal.september@gmail.com>
2020-01-15 18:43:07 -0500

    working with the solution to the file system question...

--- 07_object-oriented_design/11_file_system/file_system.py
+++ 07_object-oriented_design/11_file_system/file_system.py
@@ -6,6 +6,14 @@ in-memory file system. Illustrate with an example in code where possible.
 # What is the relationship between files and directories?
 
 
+class entry():
+    def __init__(self, name, date_created, date_modified, parent):
+        self.name = name
+        self.date_created = date_created
+        self.date_modified = date_modified
+        self.parent = parent
+
+
 class file():
     def __init__(self, name, directory):
         self.name = name
