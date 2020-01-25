f9fb0811e89279f1de0cae2e676ed28576561c4d
KQ <eternal.september@gmail.com>
2020-01-25 11:51:22 -0500

    working on it

--- 07_object-oriented_design/11_file_system/file_system_tb.py
+++ 07_object-oriented_design/11_file_system/file_system_tb.py
@@ -2,12 +2,14 @@ from file_system import *
 
 
 print('Entry functions.')
-entry_1 = Entry("entry_1")
+entry_1 = Entry("entry_1", None)
+print(entry_1)
 print()
 
 
 print('File functions.')
-file_1 = File("file_1")
+file_1 = File("file_1", None, 32)
+print(file_1)
 print()
 
 
