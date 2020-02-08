a8ce87bf3d2dcb4f752ece7ebbe47135f4fe0e0e
KQ <eternal.september@gmail.com>
2020-02-08 14:20:47 -0500

    function for displaying test info

--- 07_object-oriented_design/11_file_system/file_system_tb.py
+++ 07_object-oriented_design/11_file_system/file_system_tb.py
@@ -1,11 +1,20 @@
 from file_system import *
 
 
+def print_info(item, new_name=None):
+    print(item)
+    print()
+    if new_name is not None:
+        item.rename(new_name)
+        print(item)
+        print()	
+
+
+
 print('Entry functions.')
 entry_1 = Entry("entry_1", None)
 print(entry_1)
 print()
-
 
 print('File functions.')
 file_1 = File("file_1", None, 32)
