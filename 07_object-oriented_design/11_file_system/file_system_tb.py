7704ce9b94614525d15bbcce06b260e6a99a3985
KQ <eternal.september@gmail.com>
2020-02-09 16:25:01 -0500

    using test functions

--- 07_object-oriented_design/11_file_system/file_system_tb.py
+++ 07_object-oriented_design/11_file_system/file_system_tb.py
@@ -13,17 +13,14 @@ def print_info(item, new_name=None):
 
 print('Entry functions.')
 entry_1 = Entry("entry_1", None)
-print(entry_1)
-print()
+print_info(entry_1)
 
 print('File functions.')
 file_1 = File("file_1", None, 32)
-print(file_1)
-print()
+print_info(file_1)
 
 print('Directory functions.')
 dir_1 = Directory("dir_1", None)
-print(dir_1)
-print()
+print_info(dir_1)
 
 
