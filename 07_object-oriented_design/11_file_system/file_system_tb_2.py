40e792c892134f1318576bc557c327bee456bc9a
KQ <eternal.september@gmail.com>
2020-03-13 12:46:09 -0400

    separating out test for file functions

--- 07_object-oriented_design/11_file_system/file_system_tb_2.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_2.py
@@ -4,11 +4,11 @@ from file_system import *
 
 
 dir_1 = Directory("dir_1", None)
-file_1 = File("file_1", dir_1)
 
-# get size
-# write to file
-# get size again
+file_1 = File("file_1", dir_1)
+print(file_1.get_size())
+file_1.set_content('stuff in file_1')
+print(file_1.get_size())
 
 
 
