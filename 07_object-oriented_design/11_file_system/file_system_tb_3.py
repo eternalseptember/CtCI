c8615cf91ab4d3fa0a5733fd250d38baa656c019
KQ <eternal.september@gmail.com>
2020-03-14 19:50:00 -0400

    testing duplicate file names

---
+++ 07_object-oriented_design/11_file_system/file_system_tb_3.py
@@ -0,0 +1,14 @@
+# Testing basic file functions
+
+from file_system import *
+
+
+dir_1 = Directory("dir_1", None)
+
+file_1 = File("file_1", dir_1)
+file_2 = File("file_1", dir_1)
+# test duplicate file names?
+
+dir_1.get_contents()
+
+
