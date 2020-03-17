7a77d6937a37b0dca997d7e5e7cd91e939d4c58c
KQ <eternal.september@gmail.com>
2020-03-17 13:10:56 -0400

    working on testing dir size

--- 07_object-oriented_design/11_file_system/file_system_tb_3.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_3.py
@@ -6,10 +6,14 @@ from file_system import *
 dir_1 = Directory("dir_1", None)
 
 file_1 = File("file_1", dir_1)
-file_2 = File("file_1", dir_1)
-# test duplicate file names?
+file_2 = File("file_2", dir_1)
 
 dir_1.get_contents()
+
+
+
+print('Size of dir_1:')
+print(dir_1.get_size())
 
 
 """
