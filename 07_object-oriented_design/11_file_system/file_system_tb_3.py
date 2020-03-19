802f89b14fd86aa5bba210d29c56cd07c1a8b58d
KQ <eternal.september@gmail.com>
2020-03-19 11:16:36 -0400

    fleshed out third test case

--- 07_object-oriented_design/11_file_system/file_system_tb_3.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_3.py
@@ -4,21 +4,33 @@ from file_system import *
 
 
 dir_1 = Directory("dir_1", None)
-
 file_1 = File("file_1", dir_1)
 file_2 = File("file_2", dir_1)
 
+print('Contents of dir_1:')
 dir_1.get_contents()
+print()
 
 
 print('Size of dir_1:')
 print(dir_1.get_size())
+print()
 
 
 print('Writing to files:')
 file_1.set_content('hello world!')
 file_2.set_content('my name is __.')
+print(file_1.get_content())
+print(file_2.get_content())
+print()
 
+
+print('Size of file_1:')
+print(file_1.get_size())
+print('Size of file_2:')
+print(file_2.get_size())
+print('Total size of dir_1:')
+print(dir_1.get_size())
 
 
 
