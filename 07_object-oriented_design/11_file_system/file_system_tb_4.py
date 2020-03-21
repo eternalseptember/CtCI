347f9f13381c71db3856d1bb55ddec610d983306
KQ <eternal.september@gmail.com>
2020-03-21 17:13:07 -0400

    new test file

---
+++ 07_object-oriented_design/11_file_system/file_system_tb_4.py
@@ -0,0 +1,17 @@
+# Testing folder size after deleting file.
+
+from file_system import *
+
+
+dir_1 = Directory("dir_1", None)
+file_1 = File("file_1", dir_1)
+file_2 = File("file_2", dir_1)
+file_3 = File("file_3", dir_1)
+file_4 = File("file_4", dir_1)
+
+
+
+
+
+
+
