27b117dbe06ec841a0085c7153fc113d394a9c89
KQ <eternal.september@gmail.com>
2020-03-28 13:42:56 -0400

    continuing to set up test file

--- 07_object-oriented_design/11_file_system/file_system_tb_5.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_5.py
@@ -5,7 +5,14 @@ from file_system import *
 
 dir_1 = Directory("dir_1", None)
 file_1 = File("file_1", dir_1)
+file_2 = File("file_2", dir_1)
 
+
+print('New:')
+print(dir_1.get_full_path())
+print(file_1.get_full_path())
+print(file_2.get_full_path())
+print()
 
 
 
