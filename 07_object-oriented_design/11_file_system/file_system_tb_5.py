1dfbb6ea7f06708722a827fa08850fa6032d7b68
KQ <eternal.september@gmail.com>
2020-03-29 17:34:05 -0400

    filling out test case

--- 07_object-oriented_design/11_file_system/file_system_tb_5.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_5.py
@@ -15,5 +15,21 @@ print(file_2.get_full_path())
 print()
 
 
+print('Contents of dir_1:')
+dir_1.get_contents()
+print()
+
+
+
+print('Size of dir_1:')
+print(dir_1.get_size())
+print()
+
+
+
+print('Writing to files:')
+file_1.set_content('Day 1 of quarantine:')
+file_2.set_content('Running out of ideas')
+
 
 
