7e60b7528931efd034807bead7b7cf2d10629780
KQ <eternal.september@gmail.com>
2020-04-03 22:27:26 -0400

    starting another test case

---
+++ 07_object-oriented_design/11_file_system/file_system_tb_6.py
@@ -0,0 +1,13 @@
+# Testing the effects of renaming files and folders.
+
+from file_system import *
+
+
+dir_1 = Directory("dir_1", None)
+file_1 = File("file_1", dir_1)
+file_2 = File("file_2", dir_1)
+
+
+
+
+
