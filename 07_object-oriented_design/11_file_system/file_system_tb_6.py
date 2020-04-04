0813ed4bf506b9aa2da1c5fc1c06916426847acd
KQ <eternal.september@gmail.com>
2020-04-04 15:41:20 -0400

    filling out test case

--- 07_object-oriented_design/11_file_system/file_system_tb_6.py
+++ 07_object-oriented_design/11_file_system/file_system_tb_6.py
@@ -6,8 +6,11 @@ from file_system import *
 dir_1 = Directory("dir_1", None)
 file_1 = File("file_1", dir_1)
 file_2 = File("file_2", dir_1)
+dir_2 = Directory("dir_2", dir_1)
+dir_3 = Directory("dir_3", dir_1)
 
 
+# How many items are in dir_1?
 
 
 
